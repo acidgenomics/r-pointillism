46af5cdb3c1582dc87d0a05a459a276cea206e34
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-03 11:39:18 -0400

    Update unit test

--- tests/testthat/test_coercion_methods.R
+++ tests/testthat/test_coercion_methods.R
@@ -1,13 +1,13 @@
 context("Coercion Methods")
 
 test_that("Coerce SingleCellExperiment to seurat", {
-    x <- as(indrops_small, "seurat")
+    x <- as(sce_small, "seurat")
     expect_is(x, "seurat")
     # Check slotted count integrity
     counts <- counts(x)
     expect_is(counts, "dgCMatrix")
     expect_identical(
         dim(counts),
-        dim(indrops_small)
+        dim(sce_small)
     )
 })
