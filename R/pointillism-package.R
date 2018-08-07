8e890a5549af7fbbf64b75fc61ea616c58922f17
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-06 22:38:08 -0400

    Update ggplot2 imports

--- R/pointillism-package.R
+++ R/pointillism-package.R
@@ -36,9 +36,9 @@
 #' @importFrom DESeq2 DESeqDataSet DESeq results
 #' @importFrom edgeR calcNormFactors DGEList estimateDisp glmFit
 #' @importFrom ggplot2 aes element_blank element_rect expand_limits facet_wrap
-#'   geom_hline geom_line geom_point geom_text geom_violin ggplot guide_colorbar
-#'   guides labs scale_color_gradient scale_color_viridis_c scale_radius
-#'   scale_x_continuous scale_y_continuous theme
+#'   geom_hline geom_line geom_point geom_text geom_violin geom_vline ggplot
+#'   guide_colorbar guides labs scale_color_gradient scale_color_viridis_c
+#'   scale_radius scale_x_continuous scale_y_continuous theme
 #' @importFrom magrittr %>% set_names
 #' @importFrom Matrix rowMeans rowSums
 #' @importFrom methods as getMethod is show slot slot<- validObject
