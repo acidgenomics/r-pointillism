b15403b913b55ab9a6c0bd4aeb5a9b37df51c647
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-22 10:12:06 -0400

    Update assert check reference

--- R/plotKnownMarkersDetected-methods.R
+++ R/plotKnownMarkersDetected-methods.R
@@ -30,7 +30,7 @@ setMethod(
         headerLevel = 2L,
         ...
     ) {
-        .assertHasKnownMarkers(markers)
+        .assertIsKnownMarkersDetected(markers)
         reducedDim <- match.arg(reducedDim)
         assertIsAHeaderLevel(headerLevel)
 
