561ec0ac1e6c3bb6026ce6a9b9fae639b25526db
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-30 17:33:02 -0400

    Work on consolidating `plotFeature()` and `plotMarker()` into single file

--- R/plotMarker-methods.R
+++ R/plotMarker-methods.R
@@ -37,145 +37,311 @@ NULL
 
 
 
+.plotMarker <- function(
+    object,
+    genes,
+    reducedDim = c("TSNE", "UMAP", "PCA"),
+    expression = c("mean", "sum"),
+    color = getOption("pointillism.discrete.color", NULL),
+    pointSize = getOption("pointillism.pointSize", 0.75),
+    pointAlpha = getOption("pointillism.pointAlpha", 0.8),
+    pointsAsNumbers = getOption("pointillism.pointsAsNumbers", FALSE),
+    label = getOption("pointillism.label", TRUE),
+    labelSize = getOption("pointillism.labelSize", 6L),
+    dark = getOption("pointillism.dark", FALSE),
+    legend = getOption("pointillism.legend", TRUE),
+    title = TRUE
+) {
+    # Legacy arguments -----------------------------------------------------
+    # color
+    if (identical(color, "auto")) {
+        message("Use `color = NULL` instead of `auto`")
+        color <- NULL
+    }
+
+    # Assert checks --------------------------------------------------------
+    validObject(object)
+    assert_is_character(genes)
+    geneNames <- .mapGenesToSymbols(object, genes)
+    reducedDim <- match.arg(reducedDim)
+    expression <- match.arg(expression)
+    assertIsColorScaleContinuousOrNULL(color)
+    assert_is_a_number(pointSize)
+    assert_is_a_number(pointAlpha)
+    assert_is_a_bool(pointsAsNumbers)
+    assert_is_a_bool(label)
+    assert_is_a_number(labelSize)
+    assert_is_a_bool(dark)
+    assert_is_a_bool(legend)
+    assert_is_any_of(title, c("character", "logical", "NULL"))
+    if (is.character(title)) {
+        assert_is_a_string(title)
+    }
+
+    # Fetch reduced dimension data
+    data <- .fetchReducedDimExpressionData(
+        object = object,
+        genes = genes,
+        reducedDim = reducedDim
+    )
+    assert_is_all_of(data, "DataFrame")
+
+    # Get the axis labels.
+    axes <- colnames(data)[seq_len(2L)]
+
+    requiredCols <- c(
+        axes,
+        "centerX",
+        "centerY",
+        "ident",
+        "mean",
+        "sum",
+        "x",
+        "y"
+    )
+    assert_is_subset(requiredCols, colnames(data))
+
+    p <- ggplot(
+        data = as.data.frame(data),
+        mapping = aes(
+            x = !!sym("x"),
+            y = !!sym("y"),
+            color = !!sym(expression)
+        )
+    )
+
+    # Titles
+    subtitle <- NULL
+    if (isTRUE(title)) {
+        if (is_a_string(geneNames)) {
+            title <- geneNames
+        } else {
+            title <- NULL
+            subtitle <- geneNames
+            # Limit to the first 5 markers
+            if (length(subtitle) > 5L) {
+                subtitle <- c(subtitle[1L:5L], "...")
+            }
+            subtitle <- toString(subtitle)
+        }
+    } else if (identical(title, FALSE)) {
+        title <- NULL
+    }
+    p <- p +
+        labs(
+            x = axes[[1L]],
+            y = axes[[2L]],
+            title = title,
+            subtitle = subtitle
+        )
+
+    # Customize legend.
+    if (isTRUE(legend)) {
+        if (is_a_string(genes)) {
+            guideTitle <- "logcounts"
+        } else {
+            guideTitle <- paste0(
+                "logcounts", "\n",
+                "(", expression, ")"
+            )
+        }
+        p <- p + guides(color = guide_colorbar(title = guideTitle))
+    } else {
+        p <- p + guides(color = "none")
+    }
+
+    if (isTRUE(pointsAsNumbers)) {
+        if (pointSize < 4L) pointSize <- 4L
+        p <- p +
+            geom_text(
+                mapping = aes(
+                    x = !!sym("x"),
+                    y = !!sym("y"),
+                    label = !!sym("ident"),
+                    color = !!sym(expression)
+                ),
+                alpha = pointAlpha,
+                size = pointSize
+            )
+    } else {
+        p <- p +
+            geom_point(
+                alpha = pointAlpha,
+                size = pointSize
+            )
+    }
+
+    if (isTRUE(label)) {
+        if (isTRUE(dark)) {
+            labelColor <- "white"
+        } else {
+            labelColor <- "black"
+        }
+        p <- p +
+            geom_text(
+                mapping = aes(
+                    x = !!sym("centerX"),
+                    y = !!sym("centerY"),
+                    label = !!sym("ident")
+                ),
+                color = labelColor,
+                size = labelSize,
+                fontface = "bold"
+            )
+    }
+
+    # Dark mode.
+    if (isTRUE(dark)) {
+        p <- p + theme_midnight()
+        if (is.null(color)) {
+            color <- darkMarkerColors
+        }
+    }
+
+    if (is(color, "ScaleContinuous")) {
+        p <- p + color
+    }
+
+    p
+}
+
+
+
 #' @rdname plotMarker
 #' @export
 setMethod(
     "plotMarker",
     signature("SingleCellExperiment"),
-    function(
-        object,
-        genes,
-        reducedDim = c("TSNE", "UMAP", "PCA"),
-        expression = c("mean", "sum"),
-        color = getOption("pointillism.discrete.color", NULL),
-        pointSize = getOption("pointillism.pointSize", 0.75),
-        pointAlpha = getOption("pointillism.pointAlpha", 0.8),
-        pointsAsNumbers = getOption("pointillism.pointsAsNumbers", FALSE),
-        label = getOption("pointillism.label", TRUE),
-        labelSize = getOption("pointillism.labelSize", 6L),
-        dark = getOption("pointillism.dark", FALSE),
-        legend = getOption("pointillism.legend", TRUE),
-        title = TRUE
-    ) {
-        # Legacy arguments -----------------------------------------------------
-        # color
-        if (identical(color, "auto")) {
-            message("Use `color = NULL` instead of `auto`")
-            color <- NULL
-        }
+    .plotMarker
+)
 
-        # Assert checks --------------------------------------------------------
-        validObject(object)
-        assert_is_character(genes)
-        reducedDim <- match.arg(reducedDim)
-        expression <- match.arg(expression)
-        assertIsColorScaleContinuousOrNULL(color)
-        assert_is_a_number(pointSize)
-        assert_is_a_number(pointAlpha)
-        assert_is_a_bool(pointsAsNumbers)
-        assert_is_a_bool(label)
-        assert_is_a_number(labelSize)
-        assert_is_a_bool(dark)
-        assert_is_a_bool(legend)
-        assert_is_any_of(title, c("character", "logical", "NULL"))
-        if (is.character(title)) {
-            assert_is_a_string(title)
-        }
 
-        # Fetch reduced dimension data
-        data <- .fetchReducedDimExpressionData(
-            object = object,
-            genes = genes,
-            reducedDim = reducedDim
+
+#' @rdname plotMarker
+#' @export
+setMethod(
+    "plotMarker",
+    signature("seurat"),
+    getMethod("plotMarker", "SingleCellExperiment")
+)
+
+
+
+# Features =====================================================================
+# TODO Add pointsAsNumbers support.
+
+
+
+#' Plot Feature
+#'
+#' @name plotFeature
+#' @family Plot Functions
+#' @author Michael Steinbaugh
+#'
+#' @inheritParams general
+#' @param features `character`. Features to plot (e.g. gene expression, PC
+#'   scores, number of genes detected).
+#'
+#' @seealso [Seurat::FeaturePlot()].
+#'
+#' @return `ggplot` or `list`.
+#'
+#' @examples
+#' plotFeature(sce_small, features = c("nUMI", "nGene", "mitoRatio"))
+NULL
+
+
+
+# FIXME Simpilfy the formals here to match `plotReducedDim`.
+# NOTE We aren't using `title` argument here.
+# FIXME We're using continuous color here, so the formal won't match...
+.plotFeature <- function(
+    object,
+    features,
+    reducedDim,
+    color,
+    pointSize,
+    pointAlpha,
+    pointsAsNumbers,
+    label,
+    labelSize,
+    dark,
+    legend
+) {
+    # Legacy arguments ---------------------------------------------------------
+    # color
+    if (identical(color, "auto")) {
+        warning("Use `NULL` instead of `\"auto\"` for `color`")
+        color <- NULL
+    }
+
+    # Assert checks ------------------------------------------------------------
+    assert_is_character(features)
+    # Sanitize input to camel case.
+    features <- camel(features)
+    reducedDim <- match.arg(reducedDim)
+    assertIsColorScaleContinuousOrNULL(color)
+    assert_is_a_number(pointSize)
+    assert_is_a_number(pointAlpha)
+    assert_is_a_bool(pointsAsNumbers)
+    assert_is_a_bool(label)
+    assert_is_a_number(labelSize)
+    assert_is_a_bool(dark)
+    assert_is_a_bool(legend)
+
+    # Dark mode setup.
+    if (isTRUE(dark)) {
+        fill <- "black"
+    } else {
+        fill <- "white"
+    }
+
+    data <- .fetchReducedDimData(
+        object = object,
+        reducedDim = reducedDim
+    )
+
+    # Label the axes.
+    axes <- colnames(data)[seq_len(2L)]
+
+    # If the features are not defined, attempt to merge all reduced dims
+    # information before stopping
+    if (!all(features %in% colnames(data))) {
+        reducedDimsData <- do.call(
+            what = cbind,
+            args = reducedDims(object)
         )
-        assert_is_all_of(data, "DataFrame")
+        stopifnot(identical(rownames(data), rownames(reducedDimsData)))
+        reducedDimsData <- camel(reducedDimsData)
+        data <- data %>%
+            .[, setdiff(colnames(.), colnames(reducedDimsData))] %>%
+            cbind(reducedDimsData)
+    }
+    assert_is_subset(features, colnames(data))
 
-        # Get the axis labels.
-        axes <- colnames(data)[seq_len(2L)]
+    # FIXME Need to add pointsAsNumbers support here
+    if (isTRUE(pointsAsNumbers)) {
+        stop("pointsAsNumbers isn't supported yet")
+    }
 
-        requiredCols <- c(
-            axes,
-            "centerX",
-            "centerY",
-            "ident",
-            "mean",
-            "sum",
-            "x",
-            "y"
-        )
-        assert_is_subset(requiredCols, colnames(data))
-
+    plotlist <- lapply(features, function(feature) {
         p <- ggplot(
-            data = as.data.frame(data),
+            data = data,
             mapping = aes(
                 x = !!sym("x"),
                 y = !!sym("y"),
-                color = !!sym(expression)
+                color = !!sym(feature)
             )
-        )
-
-        # FIXME Map the genes to symbols, if defined.
-
-        # Titles
-        subtitle <- NULL
-        if (isTRUE(title)) {
-            if (is_a_string(genes)) {
-                title <- genes
-            } else {
-                title <- NULL
-                subtitle <- genes
-                # Limit to the first 5 markers
-                if (length(subtitle) > 5L) {
-                    subtitle <- c(subtitle[1L:5L], "...")
-                }
-                subtitle <- toString(subtitle)
-            }
-        } else if (identical(title, FALSE)) {
-            title <- NULL
-        }
-        p <- p +
+        ) +
+            geom_point(
+                alpha = pointAlpha,
+                size = pointSize
+            ) +
             labs(
                 x = axes[[1L]],
                 y = axes[[2L]],
-                title = title,
-                subtitle = subtitle
+                title = feature
             )
-
-        # Customize legend.
-        if (isTRUE(legend)) {
-            if (is_a_string(genes)) {
-                guideTitle <- "logcounts"
-            } else {
-                guideTitle <- paste0(
-                    "logcounts", "\n",
-                    "(", expression, ")"
-                )
-            }
-            p <- p + guides(color = guide_colorbar(title = guideTitle))
-        } else {
-            p <- p + guides(color = "none")
-        }
-
-        if (isTRUE(pointsAsNumbers)) {
-            if (pointSize < 4L) pointSize <- 4L
-            p <- p +
-                geom_text(
-                    mapping = aes(
-                        x = !!sym("x"),
-                        y = !!sym("y"),
-                        label = !!sym("ident"),
-                        color = !!sym(expression)
-                    ),
-                    alpha = pointAlpha,
-                    size = pointSize
-                )
-        } else {
-            p <- p +
-                geom_point(
-                    alpha = pointAlpha,
-                    size = pointSize
-                )
-        }
 
         if (isTRUE(label)) {
             if (isTRUE(dark)) {
@@ -196,7 +362,6 @@ setMethod(
                 )
         }
 
-        # Dark mode.
         if (isTRUE(dark)) {
             p <- p + theme_midnight()
             if (is.null(color)) {
@@ -208,16 +373,51 @@ setMethod(
             p <- p + color
         }
 
+        if (!isTRUE(legend)) {
+            p <- p + guides(color = "none")
+        }
+
         p
+    })
+
+    # Return ---------------------------------------------------------------
+    if (length(features) > 1L) {
+        plot_grid(plotlist = plotlist) +
+            theme(
+                plot.background = element_rect(color = NA, fill = fill)
+            )
+    } else {
+        plotlist[[1L]]
     }
+}
+
+# Set the formals.
+f <- formals(.plotFeature)
+fshared <- formals(.plotMarker)[
+    intersect(
+        formalArgs(.plotFeature),
+        formalArgs(.plotMarker)
+    )]
+f[names(fshared)] <- fshared
+formals(.plotFeature) <- as.pairlist(f)
+
+
+
+
+#' @rdname plotFeature
+#' @export
+setMethod(
+    "plotFeature",
+    signature("SingleCellExperiment"),
+    .plotFeature
 )
 
 
 
-#' @rdname plotMarker
+#' @rdname plotFeature
 #' @export
 setMethod(
-    "plotMarker",
+    "plotFeature",
     signature("seurat"),
-    getMethod("plotMarker", "SingleCellExperiment")
+    getMethod("plotFeature", "SingleCellExperiment")
 )
