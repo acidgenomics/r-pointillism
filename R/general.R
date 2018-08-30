f3febb9c6423c770e541a73c5d63ebffd0501ecd
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-30 15:46:34 -0400

    Fix general documentation

--- R/general.R
+++ R/general.R
@@ -14,14 +14,6 @@
 #'   recommend using [ggplot2::scale_color_manual()].
 #'   To set the discrete color palette globally, use
 #'   `options(bcbio.discrete.color = scale_color_viridis_d())`.
-#' @param fill `ggproto`/`ScaleDiscrete` or `NULL`. Desired ggplot2 fill scale.
-#'   Must supply discrete values. When set to `NULL`, the default ggplot2 color
-#'   palette will be used. If manual color definitions are desired, we recommend
-#'   using [ggplot2::scale_fill_manual()].
-#'   To set the discrete fill palette globally, use
-#'   `options(bcbio.discrete.fill = scale_fill_viridis_d())`.
-#' @param gene2symbol `data.frame`. Gene-to-symbol mappings. Columns must
-#'   contain `geneID` and `geneName`.
 #' @param dark `boolean`. Plot against a dark background using
 #'   [basejump::theme_midnight()].
 #' @param dimsUse `integer`. Vector of length 2 that denotes the columns from
@@ -30,8 +22,12 @@
 #' @param dir `string`. Output directory path.
 #' @param expression `string`. Calculation to apply. Uses [match.arg()] and
 #'   defaults to the first argument in the `character` vector.
-#' @param gene2symbol `boolean`. Automatically interconvert gene identifiers
-#'   and gene names in plots. Useful for marker plots.
+#' @param fill `ggproto`/`ScaleDiscrete` or `NULL`. Desired ggplot2 fill scale.
+#'   Must supply discrete values. When set to `NULL`, the default ggplot2 color
+#'   palette will be used. If manual color definitions are desired, we recommend
+#'   using [ggplot2::scale_fill_manual()].
+#'   To set the discrete fill palette globally, use
+#'   `options(bcbio.discrete.fill = scale_fill_viridis_d())`.
 #' @param genes `character`. Gene identifiers. Must match the rownames of the
 #'   object.
 #' @param geom `string`. Plot type. Uses [match.arg()] and defaults to the first
