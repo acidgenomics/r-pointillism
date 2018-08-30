027ba65fcb044c69d75df2ee9f3a25fa264b3ab6
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-30 10:23:55 -0400

    Improve dynamic gene-to-symbol handling

--- R/gene2symbol-internal.R
+++ R/gene2symbol-internal.R
@@ -1,5 +1,6 @@
 # Handles either gene IDs or gene names (symbols).
 .mapGenesToRownames <- function(object, genes) {
+    error <- "Failed to map genes"
     genes <- as.character(genes)
 
     # Return if the IDs match.
@@ -7,15 +8,41 @@
         return(genes)
     }
 
-    # Look to see if we need to map symbols to gene IDs.
-    g2s <- gene2symbol(object)
-    rr <- rowRanges(object)
-    any(duplicated(mcols(rr)[["geneName"]]))
-    any(duplicated(g2s[["geneName"]]))
-    # FIXME Make sure gene2symbol returns unique symbols
+    # Get the gene-to-symbol mappings.
+    # Note that gene symbols must be unique here.
+    g2s <- gene2symbol(object, unique = TRUE)
+    if (is.null(g2s)) {
+        stop(error)
+    }
 
+    # Require that the gene2symbol rownames are identical to the object.
+    # We'll be using these later for the remapping, so this step is important.
+    assert_are_identical(rownames(g2s), rownames(object))
 
-    stop("Failed to map genes")
+    # Attempt to detect symbols in the `genes` input vector, and map to gene IDs
+    # in the rows. If we detect this, make sure all of them match.
+    if (any(genes %in% g2s[["geneName"]])) {
+        assert_is_subset(genes, g2s[["geneName"]])
+        # Get the corresponding gene IDs and check against rownames.
+        match <- match(x = genes, table = g2s[["geneName"]])
+        assert_all_are_not_na(match)
+        genes <- rownames(g2s[match, , drop = FALSE])
+        return(genes)
+    }
+
+    # Alternatively, the user may be passing in gene IDs for an object with
+    # gene symbols as the rownames. This step isn't common but we'll support it
+    # and warn the user.
+    if (any(genes %in% g2s[["geneID"]])) {
+        assert_is_subset(genes, g2s[["geneID"]])
+        # Get the corresponding gene names and check against rownames.
+        match <- match(x = genes, table = g2s[["geneID"]])
+        assert_all_are_not_na(match)
+        genes <- rownames(g2s[match, , drop = FALSE])
+        return(genes)
+    }
+
+    stop(error)
 }
 
 
