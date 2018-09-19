fc4450cdd88c7ac3812d7104cb2266142afd597e
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-19 10:06:40 -0400

    Resave internal data with better attributes

--- data-raw/cell_markers.R
+++ data-raw/cell_markers.R
@@ -34,22 +34,29 @@ gs <- gs_key("1qA5ktYeimNGpZF1UPSQZATbpzEqgyxN6daoMOjv6YYw")
 ws <- gs_ws_ls(gs)
 print(ws)
 
-cell_cycle_markers <- lapply(ws, function(ws) {
-    data <- gs %>%
-        gs_read(ws = ws) %>%
-        select(phase, geneID) %>%
-        mutate(
-            geneName = convertGenesToSymbols(
-                geneID,
-                organism = ws,
-                release = release
-            )
-        ) %>%
-        group_by(phase) %>%
-        arrange(geneID, .by_group = TRUE)
-    data <- .stashAttributes(data, release = release)
-    new("CellCycleMarkers", data)
-})
+cell_cycle_markers <- lapply(
+    X = ws,
+    FUN = function(ws) {
+        data <- gs %>%
+            gs_read(ws = ws) %>%
+            select(phase, geneID) %>%
+            mutate(
+                geneName = convertGenesToSymbols(
+                    geneID,
+                    organism = ws,
+                    release = release
+                )
+            ) %>%
+            group_by(phase) %>%
+            arrange(geneID, .by_group = TRUE)
+        data <- .stashAttributes(
+            object = data,
+            organism = gsub("_", " ", ws),
+            release = release
+        )
+        new("CellCycleMarkers", data)
+    }
+)
 names(cell_cycle_markers) <- camel(ws)
 
 # Cell type markers ============================================================
@@ -62,22 +69,29 @@ ws <- gs_ws_ls(gs) %>%
     .[!str_detect(., "^_")]
 print(ws)
 
-cell_type_markers <- lapply(ws, function(ws) {
-    data <- gs %>%
-        gs_read(ws = ws) %>%
-        select(cellType, geneID) %>%
-        mutate(
-            geneName = convertGenesToSymbols(
-                geneID,
-                organism = ws,
-                release = release
-            )
-        ) %>%
-        group_by(cellType) %>%
-        arrange(geneID, .by_group = TRUE)
-    data <- .stashAttributes(data, release = release)
-    new("CellTypeMarkers", data)
-})
+cell_type_markers <- lapply(
+    X = ws,
+    FUN = function(ws) {
+        data <- gs %>%
+            gs_read(ws = ws) %>%
+            select(cellType, geneID) %>%
+            mutate(
+                geneName = convertGenesToSymbols(
+                    geneID,
+                    organism = ws,
+                    release = release
+                )
+            ) %>%
+            group_by(cellType) %>%
+            arrange(geneID, .by_group = TRUE)
+        data <- .stashAttributes(
+            object = data,
+            organism = gsub("_", " ", ws),
+            release = release
+        )
+        new("CellTypeMarkers", data)
+    }
+)
 names(cell_type_markers) <- camel(ws)
 
 # Save R data ==================================================================
