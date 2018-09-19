e0870be0998c2925f85eb6962d16173eb572648e
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-19 10:34:36 -0400

    Draft commit of `CellCycleMarkers()` generator function

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,13 @@
+# TODO Consider defining classes for all markers and pairwise DE separately.
+# TODO Rename `SingleCellMarkers` to `SeuratMarkers`?
+
+
+
+# package_version ==============================================================
+setOldClass(Classes = class(packageVersion("base")))
+
+
+
 # tibble =======================================================================
 setOldClass(Classes = class(tibble()))
 setOldClass(Classes = c("grouped_df", class(tibble())))
@@ -10,12 +20,65 @@ setOldClass(Classes = c("grouped_df", class(tibble())))
 #' @family S4 Object
 #' @author Michael Steinbaugh
 #' @export
+#'
+#' @inheritParams general
+#' @slot version `package_version`.
+#' @slot organism `string`. Full Latin organism name.
+#' @slot ensemblRelease `scalar integer`. Ensembl release version.
+#' @slot date `Date`. Date the object was saved.
+#'
 setClass(
     Class = "CellCycleMarkers",
-    contains = "grouped_df"
+    contains = "grouped_df",
+    slots = list(
+        version = "package_version",
+        organism = "character",
+        ensemblRelease = "integer",
+        date = "Date"
+    ),
+    prototype = prototype(
+        version = packageVersion("pointillism"),
+        date = Sys.Date()
+    )
 )
 
-# FIXME Add validity
+#' `CellCycleMarkers` Generator
+#'
+#' @inheritParams general
+#' @param data `grouped_df`. Grouped by `phase` column.
+#'
+#' @export
+CellCycleMarkers <-  # nolint
+    function(
+        data,
+        organism,
+        ensemblRelease
+    ) {
+        new(
+            Class = "CellCycleMarkers",
+            data,
+            organism = organism,
+            ensemblRelease = ensemblRelease
+        )
+    }
+
+setValidity(
+    Class = "CellCycleMarkers",
+    method = function(object) {
+        assert_is_all_of(object, "grouped_df")
+        assert_has_rows(object)
+        assert_are_identical(
+            x = colnames(object),
+            y = c("phase", "geneID", "geneName")
+        )
+        assert_are_identical(group_vars(object), "phase")
+        assert_is_subset(
+            x = c("version", "organism", "ensemblRelease", "date"),
+            y = names(attributes(object))
+        )
+        TRUE
+    }
+)
 
 
 
@@ -30,7 +93,23 @@ setClass(
     contains = "grouped_df"
 )
 
-# FIXME Add validity
+setValidity(
+    Class = "CellTypeMarkers",
+    method = function(object) {
+        assert_is_all_of(object, "grouped_df")
+        assert_has_rows(object)
+        assert_are_identical(
+            x = colnames(object),
+            y = c("cellType", "geneID", "geneName")
+        )
+        assert_are_identical(group_vars(object), "cellType")
+        assert_is_subset(
+            x = c("version", "organism", "ensemblRelease", "date"),
+            y = names(attributes(object))
+        )
+        TRUE
+    }
+)
 
 
 
@@ -49,13 +128,84 @@ setClass(
 #' @inheritParams general
 #' @slot data `grouped_df`.
 #' @slot rowRanges `GRanges`.
+#' @slot generator `string`. Package name and version used to generate the
+#'   markers.
 setClass(
     Class = "SingleCellMarkers",
     slots = c(
         data = "tbl_df",
-        rowRanges = "GRanges"
+        rowRanges = "GRanges",
+        zeroWeights = "character",
+        caller = "character",
+        version = "package_version",
+        organism = "character",
+        ensemblRelease = "integer",
+        date = "Date"
     ),
-    prototype = list(
-        rowRanges = GRanges()
+    prototype = prototype(
+        zeroWeights = character(),
+        version = packageVersion("pointillism"),
+        date = Sys.Date()
     )
 )
+
+# FIXME Require information on the Ensembl release used.
+
+setValidity(
+    Class = "SingleCellMarkers",
+    method = function(object) {
+        .assertIsKnownMarkers(object)
+        requiredCols <- c(
+            "cellType",  # bcbio
+            "cluster",   # Seurat
+            "geneID",    # bcbio
+            "geneName",  # bcbio
+            "avgLogFC",  # Seurat v2.1
+            "padj"       # Seurat v2.1
+        )
+        assert_is_subset(requiredCols, colnames(object))
+        TRUE
+    }
+)
+
+
+
+.isSanitizedMarkers <- function(
+    object,
+    package = "Seurat"
+) {
+    package <- match.arg(package)
+
+    # General checks -----------------------------------------------------------
+    if (!is(object, "grouped_df")) {
+        return(FALSE)
+    } else if (
+        is.null(attr(object, "vars")) ||
+        attr(object, "vars") != "cluster"
+    ) {
+        return(FALSE)
+    } else if (!"geneID" %in% colnames(object)) {
+        return(FALSE)
+    }
+
+    # Package-specific checks --------------------------------------------------
+    if (package == "Seurat") {
+        # Check for `Seurat::FindAllMarkers()` return.
+        # These columns are output in an inconsistent format, so we'll sanitize
+        # into lowerCamelCase.
+        seuratBlacklist <- c(
+            "avg_diff",   # Legacy, now "avg_logFC"
+            "avg_logFC",  # Renamed in v2.1
+            "gene",
+            "p_val",      # We'll rename to pvalue, matching DESeq2
+            "p_val_adj",  # New in v2.1, we'll rename to padj, matching DESeq2
+            "pct.1",
+            "pct.2"
+        )
+        if (any(seuratBlacklist %in% colnames(object))) {
+            return(FALSE)
+        } else {
+            return(TRUE)
+        }
+    }
+}
