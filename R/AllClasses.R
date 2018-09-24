a0cfafc62bb3bccd85e4d5cdaba43ec8723bd93d
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-23 22:31:51 -0400

    Move draft validity checks back to AllClasses

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -25,6 +25,29 @@ setClass(
 
 
 
+setValidity(
+    Class = "CellCycleMarkers",
+    method = function(object) {
+        assert_is_all_of(object, "DataFrame")
+        assert_has_rows(object)
+        assert_are_identical(
+            x = lapply(object, class),
+            y = list(
+                phase = "factor",
+                geneID = "character",
+                geneName = "character"
+            )
+        )
+        assert_is_subset(
+            x = c("version", "organism", "ensemblRelease", "date"),
+            y = names(metadata(object))
+        )
+        TRUE
+    }
+)
+
+
+
 # CellTypeMarkers ==============================================================
 #' `CellTypeMarkers` Class
 #'
@@ -39,6 +62,29 @@ setClass(
 setClass(
     Class = "CellTypeMarkers",
     contains = "DataFrame"
+)
+
+
+
+setValidity(
+    Class = "CellTypeMarkers",
+    method = function(object) {
+        assert_is_all_of(object, "DataFrame")
+        assert_has_rows(object)
+        assert_are_identical(
+            x = lapply(object, class),
+            y = list(
+                cellType = "factor",
+                geneID = "character",
+                geneName = "character"
+            )
+        )
+        assert_is_subset(
+            x = c("version", "organism", "ensemblRelease", "date"),
+            y = names(metadata(object))
+        )
+        TRUE
+    }
 )
 
 
@@ -60,6 +106,84 @@ setClass(
 
 
 
+setValidity(
+    Class = "SeuratMarkers",
+    method = function(object) {
+        # data <- slot(object, name = "data")
+        # # `FindAllMarkers()`
+        # if ("cluster" %in% colnames(data)) {
+        #     clusters <- data[["cluster"]]
+        #     assert_is_factor(clusters)
+        #     clusters <- levels(clusters)
+        #     # Ensure that we haven't already defined `ident`.
+        #     assert_are_identical(
+        #         x = clusters,
+        #         y = as.character(seq(from = 0L, to = length(clusters) - 1L))
+        #     )
+        # }
+
+        # .assertIsKnownMarkers(object)
+        # requiredCols <- c(
+        #     "cellType",  # bcbio
+        #     "cluster",   # Seurat
+        #     "geneID",    # bcbio
+        #     "geneName",  # bcbio
+        #     "avgLogFC",  # Seurat v2.1
+        #     "padj"       # Seurat v2.1
+        # )
+        # assert_is_subset(requiredCols, colnames(object))
+        #
+        #
+        #
+        # .isSanitizedMarkers <- function(
+        #     object,
+        #     package = "Seurat"
+        # ) {
+        #     package <- match.arg(package)
+        #
+        #     # General checks -----------------------------------------------------------
+        #     if (!is(object, "DataFrame")) {
+        #         return(FALSE)
+        #     } else if (
+        #         is.null(attr(object, "vars")) ||
+        #         attr(object, "vars") != "cluster"
+        #     ) {
+        #         return(FALSE)
+        #     } else if (!"geneID" %in% colnames(object)) {
+        #         return(FALSE)
+        #     }
+        #
+        #     # Package-specific checks --------------------------------------------------
+        #     if (package == "Seurat") {
+        #         # Check for `Seurat::FindAllMarkers()` return.
+        #         # These columns are output in an inconsistent format, so we'll sanitize
+        #         # into lowerCamelCase.
+        #         seuratBlacklist <- c(
+        #             "avg_diff",   # Legacy, now "avg_logFC"
+        #             "avg_logFC",  # Renamed in v2.1
+        #             "gene",
+        #             "p_val",      # We'll rename to pvalue, matching DESeq2
+        #             "p_val_adj",  # New in v2.1, we'll rename to padj, matching DESeq2
+        #             "pct.1",
+        #             "pct.2"
+        #         )
+        #         if (any(seuratBlacklist %in% colnames(object))) {
+        #             return(FALSE)
+        #         } else {
+        #             return(TRUE)
+        #         }
+        #     }
+        # }
+        #
+        #
+        #
+
+        TRUE
+    }
+)
+
+
+
 # KnownSeuratMarkers ===========================================================
 #' `KnownSeuratMarkers` Class
 #'
@@ -75,3 +199,13 @@ setClass(
     Class = "KnownSeuratMarkers",
     contains = "SeuratMarkers"
 )
+
+
+
+# FIXME
+# KnownSeuratMarkers
+# assert_are_identical(
+#     x = group_vars(markers),
+#     y = "cluster"
+# )
+# assert_is_subset(c("cellType", "name"), colnames(name))
