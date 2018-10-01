64986cc32b0cdd5883e417020f9ea3b9350abf0d
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-30 22:32:21 -0400

    Use `colData` internally instead of `metrics`

--- R/fetchData-internal.R
+++ R/fetchData-internal.R
@@ -49,7 +49,7 @@
     # Gather into long format tibble.
     # Here we're putting the genes into a "rowname" column.
     data <- data %>%
-        as("tbl_df") %>%
+        as_tibble(rownames = "rowname") %>%
         uniteInterestingGroups(interestingGroups) %>%
         gather(
             key = "rowname",
@@ -88,28 +88,28 @@
     reducedDimData <- camel(as(reducedDimData, "DataFrame"))
 
     # Cellular barcode metrics.
-    metrics <- camel(as(metrics(object), "DataFrame"))
+    colData <- camel(colData(object))
 
     # Assert checks to make sure the cbind operation works.
     assert_are_identical(
         x = rownames(reducedDimData),
-        y = rownames(metrics)
+        y = rownames(colData)
     )
     assert_are_disjoint_sets(
         x = colnames(reducedDimData),
-        y = colnames(metrics)
+        y = colnames(colData)
     )
 
     dimCols <- colnames(reducedDimData)[dimsUse]
     assert_is_character(dimCols)
 
-    # Bind the reduced dim coordinates and metrics.
-    data <- cbind(reducedDimData, metrics)
+    # Bind the data frames.
+    data <- cbind(reducedDimData, colData)
     assert_is_all_of(data, "DataFrame")
 
     # Coerce to long format tibble.
     tbl <- data %>%
-        as("tbl_df") %>%
+        as_tibble() %>%
         camel() %>%
         # Group by ident here for center calculations.
         group_by(!!sym("ident")) %>%
