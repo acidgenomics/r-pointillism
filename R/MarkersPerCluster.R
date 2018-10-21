2276efbb67f088e6d253f5a93613ee096d5490fd
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-21 17:00:30 -0400

    Reorganize files

---
+++ R/MarkersPerCluster.R
@@ -0,0 +1,17 @@
+#' @rdname Markers
+#' @export
+MarkersPerCluster <- function(
+    object,
+    ranges,
+    alpha = 0.05
+) {
+    data <- .markers(
+        object = object,
+        ranges = ranges,
+        alpha = alpha
+    )
+    out <- split(x = data, f = data[["cluster"]], drop = FALSE)
+    names(out) <- paste0("cluster", names(out))
+    metadata(out) <- metadata(data)
+    new(Class = "MarkersPerCluster", out)
+}
