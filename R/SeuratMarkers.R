b8ab1c81a77d2f41e5fc2bd5fc42ae2b7f521db0
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-20 11:47:22 -0500

    Reorganize internal code for S4 marker classes

--- R/SeuratMarkers.R
+++ R/SeuratMarkers.R
@@ -1,6 +1,7 @@
 # FIXME This needs to error if the input data.frame contains `cluster` column.
 # TODO Consider only using `SeuratMarkers()` as a single generator but
 # returning `SeuratMarkers` or `SeuratMarkersPerCluster` automatically.
+# TODO Allow generator to work with empty ranges?
 
 
 
@@ -48,32 +49,124 @@ SeuratMarkers <- function(
     ranges,
     alpha = 0.05
 ) {
-    data <- .seuratMarkers(
-        object = object,
-        ranges = ranges,
-        alpha = alpha
+    assert_is_data.frame(object)
+    assert_has_rows(object)
+    assertHasRownames(object)
+    assert_is_all_of(ranges, "GRanges")
+    assert_is_subset(
+        x = c("geneID", "geneName"),
+        y = colnames(mcols(ranges))
     )
-    rownames(data) <- data[["name"]]
-    data[["name"]] <- NULL
-    new(Class = "SeuratMarkers", data)
+    assert_is_a_number(alpha)
+    assert_all_are_in_open_range(alpha, lower = 0L, upper = 1L)
+
+    # Detect function from column names ----------------------------------------
+    seuratMarkerCols <- c("p_val", "avg_logFC", "pct.1", "pct.2", "p_val_adj")
+    if (identical(
+        colnames(object),
+        seuratMarkerCols
+    )) {
+        perCluster <- FALSE
+        fun <- "Seurat::FindMarkers"
+    } else if (identical(
+        colnames(object),
+        c(seuratMarkerCols, "cluster", "gene")
+    )) {
+        perCluster <- TRUE
+        fun <- "Seurat::FindAllMarkers"
+    }
+    message(paste0("`", fun, "` return detected."))
+
+    # Sanitize markers ---------------------------------------------------------
+    # Coerce to tibble.
+    data <- as_tibble(object, rownames = "rowname")
+    # Standardize with camel case.
+    data <- camel(data)
+
+    # Seurat mode --------------------------------------------------------------
+    # Map the Seurat matrix rownames to `rownames` column in tibble.
+    if (grep("^Seurat", fun)) {
+        if (fun == "Seurat::FindMarkers") {
+            data <- rename(data, name = !!sym("rowname"))
+        } else if (fun == "Seurat::FindAllMarkers") {
+            data <- data %>%
+                mutate(rowname = NULL) %>%
+                rename(name = !!sym("gene"))
+        }
+
+        # Update legacy columns.
+        if ("avgDiff" %in% colnames(data)) {
+            message(paste(
+                "Renaming legacy `avgDiff` column to `avgLogFC`",
+                "(changed in Seurat v2.1)."
+            ))
+            data[["avgLogFC"]] <- data[["avgDiff"]]
+            data[["avgDiff"]] <- NULL
+        }
+
+        # Rename P value columns to match DESeq2 conventions.
+        if ("pVal" %in% colnames(data)) {
+            data[["pvalue"]] <- data[["pVal"]]
+            data[["pVal"]] <- NULL
+        }
+        if ("pValAdj" %in% colnames(data)) {
+            data[["padj"]] <- data[["pValAdj"]]
+            data[["pValAdj"]] <- NULL
+        }
+    }
+
+    # Ensure that required columns are present.
+    requiredCols <- c(
+        "name",
+        "pct1",
+        "pct2",
+        "avgLogFC",     # Seurat v2.1.
+        "padj",
+        "pvalue"        # Renamed from `p_val`.
+    )
+    assert_is_subset(requiredCols, colnames(data))
+
+    if (isTRUE(perCluster)) {
+        # `cluster` is only present in `FindAllMarkers() return`.
+        data <- data %>%
+            select(!!!syms(c("cluster", "name")), everything()) %>%
+            group_by(!!sym("cluster")) %>%
+            arrange(!!sym("padj"), .by_group = TRUE)
+    } else {
+        data <- data %>%
+            select(!!sym("name"), everything()) %>%
+            arrange(!!sym("padj"))
+    }
+
+    # Bind ranges as column ---------------------------------------------------
+    data <- as(data, "DataFrame")
+    # Require that all of the markers are defined in ranges.
+    assert_is_subset(unique(data[["name"]]), names(ranges))
+    data[["ranges"]] <- ranges[data[["name"]]]
+
+    # Add metadata and return --------------------------------------------------
+    metadata(data) <- c(
+        .prototypeMetadata,
+        list(
+            alpha = alpha,
+            sessionInfo = session_info(include_base = TRUE)
+        )
+    )
+
+    if (isTRUE(perCluster)) {
+        out <- split(x = data, f = data[["cluster"]], drop = FALSE)
+        names(out) <- paste0("cluster", names(out))
+        metadata(out) <- metadata(data)
+        new(Class = "SeuratMarkersPerCluster", out)
+    } else {
+        rownames(data) <- data[["name"]]
+        data[["name"]] <- NULL
+        new(Class = "SeuratMarkers", data)
+    }
 }
 
 
 
 #' @rdname SeuratMarkers
 #' @export
-SeuratMarkersPerCluster <- function(
-    object,
-    ranges,
-    alpha = 0.05
-) {
-    data <- .seuratMarkers(
-        object = object,
-        ranges = ranges,
-        alpha = alpha
-    )
-    out <- split(x = data, f = data[["cluster"]], drop = FALSE)
-    names(out) <- paste0("cluster", names(out))
-    metadata(out) <- metadata(data)
-    new(Class = "SeuratMarkersPerCluster", out)
-}
+SeuratMarkersPerCluster <- SeuratMarkers
