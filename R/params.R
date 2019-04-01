9b762d279d41416715934978443349f90b8480d1
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-01 15:43:19 -0400

    Update params

--- R/params.R
+++ R/params.R
@@ -3,6 +3,9 @@
 #' @name params
 #' @keywords internal
 #'
+#' @param alpha `numeric(1)`.
+#'   Adjusted P value ("alpha") cutoff. If left `NULL`, will use the cutoff
+#'   defined in the object.
 #' @param reducedDim `character(1)`.
 #'   Name of reduced dimension matrix slotted in
 #'   [`reducedDims()`][SingleCellExperiment::reducedDims].
