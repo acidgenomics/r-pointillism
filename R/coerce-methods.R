2ed8aed2ca007dd10bebc3f55e1198a011b2c05c
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-12 09:28:09 -0400

    Simplify coerce methods to support Seurat v3

--- R/coerce-methods.R
+++ R/coerce-methods.R
@@ -1,18 +1,13 @@
-# FIXME Seurat v3 now masks these S3 methods.
-
-
-
 #' Force an object to belong to a class
 #'
-#' @inherit methods::as
-#' @name as
-#' @aliases coerce
+#' @name coerce
 #' @importFrom methods coerce
 #' @exportMethod coerce
 #'
 #' @seealso
 #' - `Seurat::CreateSeuratObject()`.
-#' - `Seurat::Convert()`.
+#' - `Seurat::as.Seurat()`.
+#' - `Seurat::as.SingleCellExperiment()`.
 #'
 #' @examples
 #' data(sce, package = "acidtest")
@@ -33,7 +28,7 @@ NULL
 
 
 # CellCycleMarkers to tbl_df ===================================================
-#' @rdname as
+#' @rdname coerce
 #' @name coerce,CellCycleMarkers,tbl_df-method
 #' @section `CellCycleMarkers` to `tbl_df`:
 #' S4 coercion support for creating a `tbl_df` from `CellCycleMarkers`.
@@ -53,7 +48,7 @@ setAs(
 
 
 # CellTypeMarkers to tbl_df ====================================================
-#' @rdname as
+#' @rdname coerce
 #' @name coerce,CellTypeMarkers,tbl_df-method
 #' @section `CellTypeMarkers` to `tbl_df`:
 #' S4 coercion support for creating a `tbl_df` from `CellTypeMarkers`.
@@ -72,93 +67,8 @@ setAs(
 
 
 
-# SingleCellExperiment to Seurat ===============================================
-# FIXME Seurat 3 now masks this method.
-# Seurat:::as.Seurat.SingleCellExperiment
-.as.Seurat.SingleCellExperiment <- function(from) {
-    # Create the Seurat object.
-    args <- list(
-        raw.data = counts(from),
-        meta.data = as.data.frame(colData(from)),
-        # Already applied filtering cutoffs for cells and genes.
-        min.cells = 0L,
-        min.genes = 0L,
-        # Using the default for UMI datasets.
-        is.expr = 0L,
-        project = "pointillism"
-    )
-    to <- do.call(what = CreateSeuratObject, args = args)
-
-    # Check that the dimensions match exactly.
-    assert(identical(dim(from), dim(slot(to, "raw.data"))))
-
-    # Keep extra assays, if defined (e.g. weights).
-    assayNames <- setdiff(
-        x = assayNames(from),
-        y = c(
-            "counts",
-            "logcounts",
-            "scaleData"  # from Seurat-to-SCE.
-        )
-    )
-    assays <- assays(from)[assayNames]
-
-    # rowRanges.
-    rowRanges <- rowRanges(from)
-
-    # metadata.
-    metadata <- metadata(from)
-    metadata[["sessionInfo"]] <- session_info()
-
-    misc <- list(
-        assays = assays,
-        rowRanges = rowRanges,
-        metadata = metadata
-    )
-    misc <- Filter(Negate(is.null), misc)
-    slot(to, name = "misc") <- misc
-
-    to
-}
-
-
-
-# FIXME Rethink this
-
-#' @rdname as
-#' @name coerce,SingleCellExperiment,Seurat-method
-#'
-#' @section `SingleCellExperiment` to `Seurat`:
-#' Interally `Seurat::CreateSeuratObject` is called without applying any
-#' additional filtering cutoffs, since we have already defined them during our
-#' quality control analysis. Here we are passing the raw gene-level counts of
-#' the filtered cells into a new `Seurat` class object. Use
-#' `convertGenesToSymbols` to convert gene IDs to names (symbols).
-setAs(
-    from = "SingleCellExperiment",
-    to = "Seurat",
-    def = .as.Seurat.SingleCellExperiment
-)
-
-
-
 # Seurat to SingleCellExperiment ===============================================
-# Fast internal method, that ensures dimnames are sanitized.
-# Sanitizes legacy Seurat objects that contain Ensembl IDs in names.
-# FIXME This should be safe to deprecate.
-# Seurat:::as.SingleCellExperiment.Seurat
-.as.SingleCellExperiment.Seurat <- function(from) {  # nolint
-    validObject(from)
-    names(rownames(from@raw.data)) <- NULL
-    names(rownames(from@data)) <- NULL
-    Seurat::Convert(from = from, to = "sce")
-}
-
-
-
-# FIXME Rethink this approach
-
-#' @rdname as
+#' @rdname coerce
 #' @name coerce,Seurat,SingleCellExperiment-method
 #'
 #' @section `Seurat` to `SingleCellExperiment`:
@@ -173,40 +83,14 @@ setAs(
     from = "Seurat",
     to = "SingleCellExperiment",
     def = function(from) {
-        to <- as.SingleCellExperiment(from)
-
-        # Assays.
-        assays <- assays(to)
-        # Slot scaleData, if defined. Note that we're making the dimensions
-        # match the other count matrices here.
-        scaleData <- slot(from, "scale.data")
-        if (!is.null(scaleData)) {
-            scaleData <- scaleData[rownames(to), colnames(to)]
-            assays[["scaleData"]] <- scaleData
-        }
-        stash <- .getSeuratStash(from, "assays")
-        if (!is.null(stash)) {
-            assert(areDisjointSets(names(assays), names(stash)))
-            assays <- c(assays, stash)
-            assays(to) <- assays
-        }
-
-        # Row and column data.
-        rowRanges(to) <- rowRanges(from)
-        colData(to) <- colData(to)
-
-        # Metadata.
-        metadata(to) <- metadata(from)
-        metadata(to)[["varGenes"]] <- slot(from, "var.genes")
-
-        to
+        as.SingleCellExperiment(x = from, assay = NULL)
     }
 )
 
 
 
-# Seurat to SummarizedExperiment ===============================================
-#' @rdname as
+# Seurat to RangedSummarizedExperiment =========================================
+#' @rdname coerce
 #' @name coerce,Seurat,RangedSummarizedExperiment-method
 #'
 #' @section `Seurat` to `RangedSummarizedExperiment`:
@@ -216,15 +100,17 @@ setAs(
     from = "Seurat",
     to = "RangedSummarizedExperiment",
     def = function(from) {
-        sce <- as(from, "SingleCellExperiment")
-        rse <- as(sce, "RangedSummarizedExperiment")
-        rse
+        to <- from
+        to <- as(to, "SingleCellExperiment")
+        to <- as(to, "RangedSummarizedExperiment")
+        to
     }
 )
 
 
 
-#' @rdname as
+# Seurat to SummarizedExperiment ===============================================
+#' @rdname coerce
 #' @name coerce,Seurat,SummarizedExperiment-method
 #' @section `Seurat` to `SummarizedExperiment`:
 #' S4 coercion support for creating a `SummarizedExperiment` from a `Seurat`
@@ -233,16 +119,17 @@ setAs(
     from = "Seurat",
     to = "SummarizedExperiment",
     def = function(from) {
-        rse <- as(from, "RangedSummarizedExperiment")
-        se <- as(rse, "SummarizedExperiment")
-        se
+        to <- from
+        to <- as(to, "RangedSummarizedExperiment")
+        to <- as(to, "SummarizedExperiment")
+        to
     }
 )
 
 
 
 # SeuratMarkers to tbl_df ======================================================
-#' @rdname as
+#' @rdname coerce
 #' @name coerce,SeuratMarkers,tbl_df-method
 #' @section `SeuratMarkers` to `tbl_df`:
 #' S4 coercion support for creating a `tbl_df` from a `Markers` object.
@@ -268,7 +155,7 @@ setAs(
 
 
 # SeuratMarkersPerCluster to tbl_df ============================================
-#' @rdname as
+#' @rdname coerce
 #' @name coerce,SeuratMarkersPerCluster,tbl_df-method
 #' @section `SeuratMarkersPerCluster` to `tbl_df`:
 #' S4 coercion support for creating a `tbl_df` from `SeuratMarkersPerCluster`.
@@ -293,5 +180,59 @@ setAs(
         data <- group_by(data, !!sym("cluster"))
 
         data
+    }
+)
+
+
+
+# SingleCellExperiment to Seurat ===============================================
+#' @rdname coerce
+#' @name coerce,SingleCellExperiment,Seurat-method
+#'
+#' @section `SingleCellExperiment` to `Seurat`:
+#' Interally `Seurat::CreateSeuratObject` is called without applying any
+#' additional filtering cutoffs, since we have already defined them during our
+#' quality control analysis. Here we are passing the raw gene-level counts of
+#' the filtered cells into a new `Seurat` class object. Use
+#' `convertGenesToSymbols` to convert gene IDs to names (symbols).
+setAs(
+    from = "SingleCellExperiment",
+    to = "Seurat",
+    def = function(from) {
+        # Create the Seurat object. Note that `as.Seurat()` method requires
+        # `logcounts` to be defined in `assays()`, so we're using
+        # `CreateSeuratObject()` here instead.
+        to <- CreateSeuratObject(
+            counts = counts(from),
+            project = "pointillism",
+            assay = "RNA",
+            # Already applied filtering cutoffs for cells and genes.
+            min.cells = 0L,
+            min.features = 0L,
+            names.field = 1L,
+            names.delim = "_",
+            meta.data = as.data.frame(colData(from))
+        )
+
+        # Check that the dimensions match exactly.
+        assert(identical(x = dim(from), y = dim(to)))
+
+        # Stash rowRanges.
+        rowRanges <- rowRanges(from)
+
+        # Stash metadata.
+        metadata <- metadata(from)
+        # Update the session information.
+        metadata[["sessionInfo"]] <- session_info()
+
+        # Seurat v3 still recommends using `misc` slot.
+        misc <- list(
+            rowRanges = rowRanges,
+            metadata = metadata
+        )
+        misc <- Filter(Negate(is.null), misc)
+        slot(to, name = "misc") <- misc
+
+        to
     }
 )
