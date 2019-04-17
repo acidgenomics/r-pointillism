660b349697a9e8ae4bdd5d5fb3fc41cfa0fc95b4
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-17 10:11:39 -0400

    Ensure session info is returned

--- travis-docker.R
+++ travis-docker.R
@@ -7,6 +7,10 @@
 # https://github.com/rocker-org/rocker-versioned/issues/89
 Sys.setenv(TZ = "America/New_York")
 
+utils::sessionInfo()
+sessioninfo::session_info()
+
 rcmdcheck::rcmdcheck(args = "--no-manual", error_on = "error")
 BiocCheck::BiocCheck(`quit-with-status` = FALSE)
+
 lintr::lint_package()
