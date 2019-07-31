4cefe30f1f3804855ee6af14907ddb8af94c2b5c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-31 09:40:56 -0400

    Update unit tests

--- tests/testthat/test-plotMarker.R
+++ tests/testthat/test-plotMarker.R
@@ -27,7 +27,7 @@ with_parameters_test_that(
         invisible(capture.output(
             p <- plotKnownMarkers(
                 object = object,
-                markers = seurat_known_markers
+                markers = seuratKnownMarkers
             )
         ))
         expect_type(p, "list")
@@ -41,7 +41,7 @@ context("plotTopMarkers")
 
 with_parameters_test_that(
     "plotTopMarkers", {
-        markers <- head(seurat_all_markers, n = 2L)
+        markers <- head(seuratAllMarkers, n = 2L)
         invisible(capture.output(
             x <- plotTopMarkers(
                 object = object,
