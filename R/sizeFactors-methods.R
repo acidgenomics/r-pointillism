ba22b9bd8f92f26047387ba7afa8ed3a370ee33c
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-02 15:42:07 -0400

    Add sizeFactor support

---
+++ R/sizeFactors-methods.R
@@ -0,0 +1,55 @@
+#' Size factors
+#'
+#' @inheritParams basejump::params
+#'
+#' @name sizeFactors
+#'
+#' @return `numeric`.
+#'   Named numeric vector, corresponding to cells.
+#'
+#' @examples
+#' data(SingleCellExperiment, package = "acidtest")
+#'
+#' ## SingleCellExperiment ====
+#' object <- SingleCellExperiment
+#' object <- estimateSizeFactors(object)
+#' head(sizeFactors(object))
+NULL
+
+
+
+## Updated 2019-08-02.
+`sizeFactors,SingleCellExperiment` <-  # nolint
+    methodFunction(
+        f = "sizeFactors",
+        signature = "DESeqDataSet",
+        package = "DESeq2"
+    )
+
+
+
+#' @rdname sizeFactors
+#' @export
+setMethod(
+    f = "sizeFactors",
+    signature = signature("SingleCellExperiment"),
+    definition = `sizeFactors,SingleCellExperiment`
+)
+
+
+
+## Updated 2019-08-02.
+`sizeFactors,cell_data_set` <-  # nolint
+    function(object) {
+        monocle3::size_factors(object)
+    }
+
+
+
+#' @rdname sizeFactors
+#' @export
+setMethod(
+    f = "sizeFactors",
+    signature = signature("cell_data_set"),
+    definition = `sizeFactors,cell_data_set`
+)
