8c0e6b7ed99549d7c570949f985bb1014d3f99dc
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-02 16:42:51 -0400

    Rework to add support for cell_data_set

--- R/plotPCElbow-methods.R
+++ R/plotPCElbow-methods.R
@@ -1,4 +1,8 @@
 ## FIXME Need to add monocle3 support.
+## Unclear how Seurat 3 is calculating these values.
+## FIXME prop_varex <- cds@preprocess_aux$prop_var_expl
+## irlba_res
+## sdev^2 / sum(irlba_res$sdev^2)
 
 
 
@@ -57,141 +61,110 @@ NULL
 
 
 
-## Updated 2019-07-31.
+## Updated 2019-08-02.
+.plotPCElbow <- function(pctStdDev, minPct = 0.01, maxCumPct = 0.9) {
+    assert(
+        is.numeric(pctStdDev),
+        isNumber(minPct),
+        allAreInLeftOpenRange(
+            x = c(minPct, maxCumPct),
+            lower = 0L,
+            upper = 1L
+        )
+    )
+    cumsum <- cumsum(pctStdDev)
+    xLab <- "PCA component"
+
+    data <- tibble(
+        pc = seq_along(pctStdDev),
+        pct = pctStdDev,
+        cumsum = cumsum
+    )
+
+    minPctCutoff <- data %>%
+        .[.[["pct"]] >= minPct, "pc"] %>%
+        max()
+    maxCumPctCutoff <- data %>%
+        .[.[["cumsum"]] <= maxCumPct, "pc"] %>%
+        max()
+
+    ## Pick the smallest value of the cutoffs
+    cutoff <- min(minPctCutoff, maxCumPctCutoff)
+
+    ## Percent standard deviation ----------------------------------------------
+    ggpct <- ggplot(
+        data = data,
+        mapping = aes(
+            x = !!sym("pc"),
+            y = !!sym("pct")
+        )
+    ) +
+        geom_hline(
+            color = "orange",
+            size = 1L,
+            yintercept = minPct
+        ) +
+        geom_line() +
+        geom_point() +
+        geom_vline(xintercept = cutoff) +
+        labs(
+            x = xLab,
+            y = "% std dev"
+        ) +
+        expand_limits(y = 0L) +
+        scale_y_continuous(labels = percent)
+
+    ## Cumulative percent standard deviation -----------------------------------
+    ggcumsum <- ggplot(
+        data = data,
+        mapping = aes(
+            x = !!sym("pc"),
+            y = !!sym("cumsum")
+        )
+    ) +
+        geom_hline(
+            color = "orange",
+            size = 1L,
+            yintercept = maxCumPct
+        ) +
+        geom_line() +
+        geom_point() +
+        geom_vline(xintercept = cutoff) +
+        labs(
+            x = xLab,
+            y = "cum % std dev"
+        ) +
+        expand_limits(y = c(0L, 1L)) +
+        scale_y_continuous(labels = percent)
+
+    plotlist <- list(
+        pct = ggpct,
+        cumsum = ggcumsum
+    )
+
+    p <- plot_grid(plotlist = plotlist)
+    show(p)
+
+    invisible(seq_len(cutoff))
+}
+
+
+
+## Updated 2019-08-02.
 `plotPCElbow,Seurat` <-  # nolint
-    function(
-        object,
-        reduction = "PCA",
-        minSD = 1L,
-        minPct = 0.01,
-        maxCumPct = 0.9,
-        trans = c("identity", "sqrt")
-    ) {
-        assert(
-            isString(reduction),
-            isNumber(minSD),
-            isPositive(minSD),
-            isNumber(minPct),
-            isNumber(maxCumPct),
-            allAreInLeftOpenRange(
-                x = c(minPct, maxCumPct),
-                lower = 0L,
-                upper = 1L
-            )
+    function(object, minPct, maxCumPct) {
+        stdDev <- Stdev(object = object, reduction = "pca")
+        assert(is.numeric(stdDev))
+        pctStdDev <- stdDev ^ 2L / sum(stdDev ^ 2L)
+        .plotPCElbow(
+            pctStdDev = pctStdDev,
+            minPct = minPct,
+            maxCumPct = maxCumPct
         )
-        trans <- match.arg(trans)
-        xLab <- "PCA component"
-
-        ## dr: dimensional reduction
-        ## sdev: standard deviation
-        ## FIXME Extract the code for this...
-        ## FIXME Seurat:::Stdev.DimReduc
-        ## https://github.com/satijalab/seurat/blob/master/R/objects.R#L3386
-        sdev <- Stdev(object = object, reduction = tolower(reduction))
-        assert(is.numeric(sdev))
-        pct <- sdev ^ 2L / sum(sdev ^ 2L)
-        cumsum <- cumsum(pct)
-
-        data <- tibble(
-            pc = seq_along(sdev),
-            sdev = sdev,
-            pct = pct,
-            cumsum = cumsum
-        )
-
-        minSDCutoff <- data %>%
-            .[.[["sdev"]] >= minSD, "pc"] %>%
-            max()
-        minPctCutoff <- data %>%
-            .[.[["pct"]] >= minPct, "pc"] %>%
-            max()
-        maxCumPctCutoff <- data %>%
-            .[.[["cumsum"]] <= maxCumPct, "pc"] %>%
-            max()
-
-        ## Pick the smallest value of the cutoffs
-        cutoff <- min(minSDCutoff, minPctCutoff, maxCumPctCutoff)
-
-        ## Standard deviation --------------------------------------------------
-        ggsd <- ggplot(
-            data = data,
-            mapping = aes(
-                x = !!sym("pc"),
-                y = !!sym("sdev")
-            )
-        ) +
-            geom_hline(
-                color = "orange",
-                size = 1L,
-                yintercept = minSD
-            ) +
-            geom_line() +
-            geom_point() +
-            geom_vline(xintercept = cutoff) +
-            labs(
-                x = xLab,
-                y = "std dev"
-            ) +
-            expand_limits(y = 0L) +
-            scale_y_continuous(trans = trans)
-
-        ## Percent standard deviation ------------------------------------------
-        ggpct <- ggplot(
-            data = data,
-            mapping = aes(
-                x = !!sym("pc"),
-                y = !!sym("pct")
-            )
-        ) +
-            geom_hline(
-                color = "orange",
-                size = 1L,
-                yintercept = minPct
-            ) +
-            geom_line() +
-            geom_point() +
-            geom_vline(xintercept = cutoff) +
-            labs(
-                x = xLab,
-                y = "% std dev"
-            ) +
-            expand_limits(y = 0L) +
-            scale_y_continuous(labels = percent, trans = trans)
-
-        ## Cumulative percent standard deviation -------------------------------
-        ggcumsum <- ggplot(
-            data = data,
-            mapping = aes(
-                x = !!sym("pc"),
-                y = !!sym("cumsum")
-            )
-        ) +
-            geom_hline(
-                color = "orange",
-                size = 1L,
-                yintercept = maxCumPct
-            ) +
-            geom_line() +
-            geom_point() +
-            geom_vline(xintercept = cutoff) +
-            labs(
-                x = xLab,
-                y = "cum % std dev"
-            ) +
-            expand_limits(y = c(0L, 1L)) +
-            scale_y_continuous(labels = percent, trans = trans)
-
-        plotlist <- list(
-            sd = ggsd,
-            pct = ggpct,
-            cumsum = ggcumsum
-        )
-
-        p <- plot_grid(plotlist = plotlist)
-        show(p)
-
-        invisible(seq_len(cutoff))
     }
+
+args <- c("minPct", "maxCumPct")
+formals(`plotPCElbow,Seurat`)[args] <- formals(.plotPCElbow)[args]
 
 
 
@@ -205,12 +178,19 @@ setMethod(
 
 
 
+## Updated 2019-08-02.
 `plotPCElbow,cell_data_set` <-  # nolint
-    function(object) {
-        ## FIXME Rework this to be consistent with Seurat method.
-        ## FIXME Probably need to break out the plot code.
-        monocle3::plot_pc_variance_explained(object)
+    function(object, minPct, maxCumPct) {
+        pctStdDev <- slot(object, "preprocess_aux")[["prop_var_expl"]]
+        .plotPCElbow(
+            pctStdDev = pctStdDev,
+            minPct = minPct,
+            maxCumPct = maxCumPct
+        )
     }
+
+args <- c("minPct", "maxCumPct")
+formals(`plotPCElbow,cell_data_set`)[args] <- formals(.plotPCElbow)[args]
 
 
 
