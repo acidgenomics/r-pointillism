ebd48a8eb4efe1c0b7e547813fef5ff6dbcc2725
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-02 10:36:01 -0400

    Add support for monocle3

--- R/clusterID-methods.R
+++ R/clusterID-methods.R
@@ -1,6 +1,10 @@
+## FIXME Consider renaming to clusters from clusterID.
+
+
+
 #' @name clusterID
 #' @inherit bioverbs::clusterID
-#' @note Updated 2019-07-31.
+#' @note Updated 2019-08-02.
 #'
 #' @inheritParams basejump::params
 #' @param ... Additional arguments.
@@ -26,14 +30,86 @@ NULL
 
 
 
-## Updated 2019-07-31.
+## Updated 2019-08-02.
+`clusterID,Seurat` <-  # nolint
+    function(object) {
+        validObject(object)
+        Idents(object)
+    }
+
+
+
+#' @rdname clusterID
+#' @export
+setMethod(
+    f = "clusterID",
+    signature = signature("Seurat"),
+    definition = `clusterID,Seurat`
+)
+
+
+
+## For `reductionMethod`, note that positional scalar works.
+## Updated 2019-08-02.
+`clusterID,cell_data_set` <-  # nolint
+    function(object, reductionMethod) {
+        validObject(object)
+        assert(isScalar(reductionMethod))
+        monocle3::clusters(
+            x = object,
+            reduction_method = reductionMethod
+        )
+    }
+
+f <- methodFormals(
+    f = "clusters",
+    signature = signature(x = "cell_data_set"),
+    package = "monocle3"
+)
+formals(`clusterID,cell_data_set`)[["reductionMethod"]] <-
+    f[["reduction_method"]]
+
+
+
+#' @rdname clusterID
+#' @export
+setMethod(
+    f = "clusterID",
+    signature = signature("cell_data_set"),
+    definition = `clusterID,cell_data_set`
+)
+
+
+
+## Consider removing this fallback method support.
+## Updated 2019-08-02.
 `clusterID,SingleCellExperiment` <-  # nolint
     function(object) {
-        object <- as(object, "SingleCellExperiment")
-        x <- colData(object)[["ident"]]
-        assert(is.factor(x))
-        names(x) <- colnames(object)
-        x
+        validObject(object)
+
+        ## Look for `ident` column defined by Seurat.
+        if (isSubset("ident", colnames(colData(object)))) {
+            x <- colData(object)[["ident"]]
+            assert(is.factor(x))
+            names(x) <- colnames(object)
+            return(x)
+        }
+
+        ## Look for `clusters` slot defined by monocle3.
+        if (.hasSlot("clusters", object)) {
+            x <- slot(object, "clusters")
+            ## Here we're assuming the first slot, typically UMAP.
+            x <- x[[1L]]
+            assert(isSubset("clusters", names(x)))
+            x <- x[["clusters"]]
+            assert(
+                is.factor(x),
+                identical(names(x), colnames(object))
+            )
+            return(x)
+        }
+
+        stop("Failed to locate cluster mappings in the object.")
     }
 
 
@@ -44,20 +120,4 @@ setMethod(
     f = "clusterID",
     signature = signature("SingleCellExperiment"),
     definition = `clusterID,SingleCellExperiment`
-)
-
-
-
-## Updated 2019-07-31.
-`clusterID,Seurat` <-  # nolint
-    `clusterID,SingleCellExperiment`
-
-
-
-#' @rdname clusterID
-#' @export
-setMethod(
-    f = "clusterID",
-    signature = signature("Seurat"),
-    definition = `clusterID,Seurat`
 )
