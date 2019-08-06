1a6e66c640b73093ca325ce08e9aede67a74995b
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-06 16:27:25 -0400

    Improve clusters SCE method support

--- R/clusters-methods.R
+++ R/clusters-methods.R
@@ -5,6 +5,10 @@
 #' @inheritParams acidroxygen::params
 #' @param ... Additional arguments.
 #'
+#' @seealso
+#' - `Seurat::Idents()`.
+#' - `monocle3::clusters()`.
+#'
 #' @examples
 #' data(Seurat, package = "acidtest")
 #'
@@ -23,6 +27,29 @@ NULL
 #' @usage clusters(object, ...)
 #' @export
 NULL
+
+
+
+## Updated 2019-08-06.
+`clusters,SingleCellExperiment` <-  # nolint
+    function(object) {
+        validObject(object)
+        assert(isSubset("ident", colnames(colData(object))))
+        ident <- colData(object)[["ident"]]
+        assert(is.factor(ident))
+        names(ident) <- colnames(object)
+        ident
+    }
+
+
+
+#' @rdname clusters
+#' @export
+setMethod(
+    f = "clusters",
+    signature = signature("SingleCellExperiment"),
+    definition = `clusters,SingleCellExperiment`
+)
 
 
 
@@ -48,17 +75,16 @@ setMethod(
 ## Note that monocle3 currently defines the generic using "x" instead of
 ## "object", and requires "reduction_method" in definition.
 ##
-## For `reductionMethod`, note that positional scalar works.
+## For `reduction`, note that positional scalar works.
 ##
 ## Updated 2019-08-02.
 `clusters,cell_data_set` <-  # nolint
-    function(object, reductionMethod) {
+    function(object, reduction) {
         validObject(object)
-        assert(isScalar(reductionMethod))
-
+        assert(isScalar(reduction))
         monocle3::clusters(
             x = object,
-            reduction_method = reductionMethod
+            reduction_method = reduction
         )
     }
 
@@ -67,7 +93,7 @@ f <- methodFormals(
     signature = signature(x = "cell_data_set"),
     package = "monocle3"
 )
-formals(`clusters,cell_data_set`)[["reductionMethod"]] <-
+formals(`clusters,cell_data_set`)[["reduction"]] <-
     f[["reduction_method"]]
 
 
@@ -78,47 +104,4 @@ setMethod(
     f = "clusters",
     signature = signature("cell_data_set"),
     definition = `clusters,cell_data_set`
-)
-
-
-
-## Consider removing this fallback method support.
-## Updated 2019-08-02.
-`clusters,SingleCellExperiment` <-  # nolint
-    function(object) {
-        validObject(object)
-
-        ## Look for `ident` column defined by Seurat.
-        if (isSubset("ident", colnames(colData(object)))) {
-            x <- colData(object)[["ident"]]
-            assert(is.factor(x))
-            names(x) <- colnames(object)
-            return(x)
-        }
-
-        ## Look for `clusters` slot defined by monocle3.
-        if (.hasSlot("clusters", object)) {
-            x <- slot(object, "clusters")
-            ## Here we're assuming the first slot, typically UMAP.
-            x <- x[[1L]]
-            assert(isSubset("clusters", names(x)))
-            x <- x[["clusters"]]
-            assert(
-                is.factor(x),
-                identical(names(x), colnames(object))
-            )
-            return(x)
-        }
-
-        stop("Failed to locate cluster mappings in the object.")
-    }
-
-
-
-#' @rdname clusters
-#' @export
-setMethod(
-    f = "clusters",
-    signature = signature("SingleCellExperiment"),
-    definition = `clusters,SingleCellExperiment`
 )
