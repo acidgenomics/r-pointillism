01282852ac2fda8ce332b31ce57bd294bf551727
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-23 15:55:06 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -7,17 +7,18 @@
 #' @importClassesFrom SingleCellExperiment SingleCellExperiment
 #' @importClassesFrom monocle3 cell_data_set
 #'
-#' @importMethodsFrom basejump coerce
+#' @importMethodsFrom basejump coerce unsplit
 #'
-#' @importFrom BiocGenerics cbind counts counts<- do.call lapply rowMeans
-#'   rowSums sapply sizeFactors sizeFactors<-
+#' @importFrom BiocGenerics counts counts<- sizeFactors sizeFactors<-
 #' @importFrom BiocParallel MulticoreParam SerialParam bpparam bpprogressbar
 #'   bpprogressbar<-
 #' @importFrom DelayedArray DelayedArray
 #' @importFrom DelayedMatrixStats colSums2 rowSums2
 #' @importFrom DESeq2 DESeqDataSet DESeq estimateSizeFactorsForMatrix results
-#' @importFrom S4Vectors DataFrame as.data.frame as.matrix complete.cases head
-#'   mcols mcols<- metadata metadata<- na.omit split tail
+#' @importFrom IRanges SplitDataFrameList unsplit
+#' @importFrom S4Vectors DataFrame as.data.frame as.matrix cbind complete.cases
+#'   do.call head lapply mcols mcols<- metadata metadata<- na.omit rowMeans
+#'   rowSums sapply split tail
 #' @importFrom Seurat CreateSeuratObject Idents as.SingleCellExperiment
 #'   as.Seurat DefaultAssay GetAssayData NormalizeData Stdev VariableFeatures
 #' @importFrom SingleCellExperiment logcounts logcounts<- normcounts
@@ -28,7 +29,7 @@
 #' @importFrom acidplots acid_theme_dark acid_theme_light
 #' @importFrom basejump Gene2Symbol camelCase coerce convertGenesToSymbols
 #'   decode estimateSizeFactors gene2symbol geometricMean import
-#'   interestingGroups interestingGroups<- makeNames mapGenesToIDs
+#'   interestingGroups interestingGroups<- left_join makeNames mapGenesToIDs
 #'   mapGenesToRownames mapGenesToSymbols markdownHeader matchArgsToDoCall
 #'   matchInterestingGroups metrics organism organism<- printString sampleData
 #'   sampleData<- sampleNames separator showSlotInfo snakeCase upperCamel
@@ -47,20 +48,22 @@
 #'   isString isSubset validate
 #' @importFrom methods as getMethod is new setAs setClass setMethod
 #'   setReplaceMethod setValidity show slot slot<- validObject .hasSlot
-#' @importFrom rlang !! !!! := sym syms
+#' @importFrom rlang !! sym
 #' @importFrom scales percent pretty_breaks
 #' @importFrom scater calculateCPM normalizeSCE
 #' @importFrom sessioninfo session_info
 #' @importFrom stats median model.matrix relevel
+#' @importFrom tidyr gather
 #' @importFrom utils capture.output globalVariables packageVersion
 #'
 #'
 #'
 #' @importFrom dplyr arrange desc everything filter group_by group_vars
-#'   left_join mutate mutate_at mutate_if n pull rename select slice summarize
+#'   mutate mutate_at mutate_if n pull rename select slice summarize
 #'   ungroup vars
 #' @importFrom magrittr %>% set_names set_rownames
-#' @importFrom tidyr gather
+#' @importFrom rlang !!! := syms
+
 #' @importFrom tibble as_tibble column_to_rownames remove_rownames
 #'   rownames_to_column tibble
 NULL
