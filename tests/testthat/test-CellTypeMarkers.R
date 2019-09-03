c88a30a44d25c8adfd2652d710382e07a302374a
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-03 13:27:49 -0400

    Update unit test

--- tests/testthat/test-CellTypeMarkers.R
+++ tests/testthat/test-CellTypeMarkers.R
@@ -1,16 +1,5 @@
 context("CellTypeMarkers")
 
-## Error in validObject(.Object) :
-##   invalid class "CellTypeMarkers" object: identical(x = lapply(object[[1L]], class), y = list(cellType = "factor",  .... is not TRUE.
-## Calls: CellTypeMarkers -> new -> initialize -> initialize -> validObject
-## Backtrace:
-##     █
-##  1. └─pointillism::CellTypeMarkers(object = markers, gene2symbol = gene2symbol)
-##  2.   └─methods::new(Class = class, data) /data00/draco/acidbase/packages/pointillism/R/AllGenerators.R:114:8
-##  3.     ├─methods::initialize(value, ...)
-##  4.     └─methods::initialize(value, ...)
-##  5.       └─methods::validObject(.Object)
-
 test_that("CellTypeMarkers", {
     file <- system.file(
         "extdata/markers/cell-type/homo-sapiens.csv",
@@ -25,5 +14,5 @@ test_that("CellTypeMarkers", {
         object = markers,
         gene2symbol = gene2symbol
     )
-    expect_is(x, "CellTypeMarkers")
+    expect_s4_class(x, "CellTypeMarkers")
 })
