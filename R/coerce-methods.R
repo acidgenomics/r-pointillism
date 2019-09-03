9fa5a7e00755eb361a697de732c0717bbb4a0e28
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-03 08:10:21 -0400

    Remove coercion methods returning `tbl_df`

--- R/coerce-methods.R
+++ R/coerce-methods.R
@@ -26,54 +26,6 @@ NULL
 
 
 ## Updated 2019-07-31.
-`coerce,CellCycleMarkers,tbl_df` <-  # nolint
-    function(from) {
-        validObject(from)
-        data <- do.call(what = rbind, args = from)
-        data <- as(data, "tbl_df")
-        data <- group_by(data, !!sym("phase"))
-        data
-    }
-
-
-
-#' @rdname coerce
-#' @name coerce,CellCycleMarkers,tbl_df-method
-#' @section `CellCycleMarkers` to `tbl_df`:
-#' S4 coercion support for creating a `tbl_df` from `CellCycleMarkers`.
-setAs(
-    from = "CellCycleMarkers",
-    to = "tbl_df",
-    def = `coerce,CellCycleMarkers,tbl_df`
-)
-
-
-
-## Updated 2019-07-31.
-`coerce,CellTypeMarkers,tbl_df` <-  # nolint
-    function(from) {
-        validObject(from)
-        data <- do.call(what = rbind, args = from)
-        data <- as(data, "tbl_df")
-        data <- group_by(data, !!sym("cellType"))
-        data
-    }
-
-
-
-#' @rdname coerce
-#' @name coerce,CellTypeMarkers,tbl_df-method
-#' @section `CellTypeMarkers` to `tbl_df`:
-#' S4 coercion support for creating a `tbl_df` from `CellTypeMarkers`.
-setAs(
-    from = "CellTypeMarkers",
-    to = "tbl_df",
-    def = `coerce,CellTypeMarkers,tbl_df`
-)
-
-
-
-## Updated 2019-07-31.
 `coerce,Seurat,SingleCellExperiment` <-  # nolint
     function(from) {
         ## Using the Seurat S3 coercion method here.
@@ -159,65 +111,6 @@ setAs(
 
 
 ## Updated 2019-07-31.
-`coerce,SeuratMarkers,tbl_df` <-  # nolint
-    function(from) {
-        validObject(from)
-        ## Get gene2symbol from slotted ranges.
-        g2s <- mcols(from[["ranges"]])[c("geneID", "geneName")]
-        data <- as(from, "DataFrame")
-        data[["ranges"]] <- NULL
-        assert(areDisjointSets(colnames(data), colnames(g2s)))
-        data <- cbind(data, g2s)
-        data <- as(data, "tbl_df")
-        data
-    }
-
-
-
-#' @rdname coerce
-#' @name coerce,SeuratMarkers,tbl_df-method
-#' @section `SeuratMarkers` to `tbl_df`:
-#' S4 coercion support for creating a `tbl_df` from a `Markers` object.
-setAs(
-    from = "SeuratMarkers",
-    to = "tbl_df",
-    def = `coerce,SeuratMarkers,tbl_df`
-)
-
-
-
-## Updated 2019-07-31.
-`coerce,SeuratMarkersPerCluster,tbl_df` <-  # nolint
-    function(from) {
-        validObject(from)
-        data <- do.call(what = rbind, args = from)
-        ## Get gene2symbol from slotted ranges.
-        g2s <- mcols(data[["ranges"]])[c("geneID", "geneName")]
-        data[["ranges"]] <- NULL
-        assert(areDisjointSets(colnames(data), colnames(g2s)))
-        data <- cbind(data, g2s)
-        ## Ensure Rle columns get decoded.
-        data <- decode(data)
-        data <- as(data, "tbl_df")
-        data <- group_by(data, !!sym("cluster"))
-        data
-    }
-
-
-
-#' @rdname coerce
-#' @name coerce,SeuratMarkersPerCluster,tbl_df-method
-#' @section `SeuratMarkersPerCluster` to `tbl_df`:
-#' S4 coercion support for creating a `tbl_df` from `SeuratMarkersPerCluster`.
-setAs(
-    from = "SeuratMarkersPerCluster",
-    to = "tbl_df",
-    def = `coerce,SeuratMarkersPerCluster,tbl_df`
-)
-
-
-
-## Updated 2019-07-31.
 `coerce,SingleCellExperiment,Seurat` <-  # nolint
     function(from) {
         ## Create the Seurat object. Note that `as.Seurat()` method requires
@@ -234,18 +127,14 @@ setAs(
             names.delim = "_",
             meta.data = as.data.frame(colData(from))
         )
-
         ## Check that the dimensions match exactly.
         assert(identical(x = dim(from), y = dim(to)))
-
         ## Stash rowRanges.
         rowRanges <- rowRanges(from)
-
         ## Stash metadata.
         metadata <- metadata(from)
         ## Update the session information.
         metadata[["sessionInfo"]] <- session_info()
-
         ## Seurat v3 still recommends using `misc` slot.
         misc <- list(
             rowRanges = rowRanges,
@@ -253,7 +142,7 @@ setAs(
         )
         misc <- Filter(Negate(is.null), misc)
         slot(to, name = "misc") <- misc
-
+        ## Return.
         to
     }
 
