f3c4872242f39529589f53802c37e7c09c507ea5
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-03 10:40:39 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -40,12 +40,12 @@
 #'   geom_vline ggplot guide_colorbar guides labs scale_color_gradient
 #'   scale_color_viridis_c scale_radius scale_x_continuous scale_y_continuous
 #'   theme theme_set
-#' @importFrom goalie allAreInLeftOpenRange allAreNonNegative allArePositive
-#'   areDisjointSets areIntersectingSets areSetEqual assert bapply false isAlpha
-#'   isInt hasColnames hasLength hasNames hasRownames hasRows hasValidDimnames
-#'   hasValidNames isGGScale isAny isCharacter isFlag isHeaderLevel isIntegerish
-#'   isNonEmpty isNonNegative isNumber isPositive isScalar isScalarNumeric
-#'   isString isSubset validate
+#' @importFrom goalie allAreInLeftOpenRange allAreMatchingRegex
+#'   allAreNonNegative allArePositive areDisjointSets areIntersectingSets
+#'   areSetEqual assert bapply false isAlpha isInt hasColnames hasLength
+#'   hasNames hasRownames hasRows hasValidDimnames hasValidNames isGGScale isAny
+#'   isCharacter isFlag isHeaderLevel isIntegerish isNonEmpty isNonNegative
+#'   isNumber isPositive isScalar isScalarNumeric isString isSubset validate
 #' @importFrom methods as getMethod is new setAs setClass setMethod
 #'   setReplaceMethod setValidity show slot slot<- validObject .hasSlot
 #' @importFrom rlang !! sym
