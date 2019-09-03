9d5b7dd38e17de898dc4c38e0b77fc6b4683a336
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-03 13:29:16 -0400

    Update unit test

--- tests/testthat/test-clusterCellCountsPerSample.R
+++ tests/testthat/test-clusterCellCountsPerSample.R
@@ -3,20 +3,20 @@ context("clusterCellCountsPerSample")
 with_parameters_test_that(
     "clusterCellCountsPerSample", {
         x <- clusterCellCountsPerSample(object)
-        expect_is(x, "grouped_df")
-        expect_identical(group_vars(x), "sampleName")
+        expect_s4_class(x, "DataFrame")
         expect_identical(
             lapply(x, class),
             list(
-                sampleName = "factor",
                 ident = "factor",
+                sampleName = "factor",
                 n = "integer",
+                nPerIdent = "integer",
                 ratio = "numeric"
             )
         )
     },
     object = list(
         SingleCellExperiment = sce,
-        seurat = seurat
+        Seurat = seurat
     )
 )
