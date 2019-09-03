395c1c30207e3d354c04af15962717dac9844090
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-03 11:10:05 -0400

    Removed rlang dependency

--- R/imports.R
+++ R/imports.R
@@ -63,7 +63,6 @@ NULL
 #' @importFrom dplyr arrange desc everything filter group_by group_vars
 #'   mutate mutate_at n pull rename select slice summarize
 #'   ungroup vars
-#' @importFrom rlang !!! := syms
 #' @importFrom tidyr gather
 #' @importFrom tibble column_to_rownames remove_rownames
 #'   rownames_to_column tibble
