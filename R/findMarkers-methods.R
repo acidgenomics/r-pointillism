7c49b5110a0a04df58c4634f326d7e820bd13b53
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-30 09:20:44 -0500

    Switch to using cli for messages

--- R/findMarkers-methods.R
+++ R/findMarkers-methods.R
@@ -1,6 +1,6 @@
 #' @name findMarkers
 #' @inherit acidgenerics::findMarkers
-#' @note Updated 2019-08-02.
+#' @note Updated 2020-01-30.
 #'
 #' @inheritParams acidroxygen::params
 #' @param ... Passthrough arguments to [diffExp()].
@@ -30,9 +30,10 @@ NULL
 
 
 
-## Updated 2019-08-02.
+## Updated 2020-01-30.
 `findMarkers,SingleCellExperiment` <-  # nolint
     function(object, ...) {
+        cli_h1("{.fun findMarkers}")
         object <- as(object, "SingleCellExperiment")
         ## Get the cluster mappings. Following the Seurat nomenclature here of
         ## using "ident" to denote the cluster identifier mapping factor.
@@ -40,16 +41,13 @@ NULL
         assert(is.factor(ident), hasNames(ident))
         clusters <- levels(ident)
         assert(length(clusters) >= 2L)
-        message(sprintf("%d clusters detected.", length(clusters)))
+        cli_alert_info(sprintf("%d clusters detected.", length(clusters)))
         ## Loop across the clusters and calculate gene enrichment relative to
         ## all of the other clusters combined.
         list <- lapply(
             X = clusters,
             FUN = function(cluster) {
-                message(sprintf(
-                    "Cluster %s ====",
-                    as.character(cluster)
-                ))
+                cli_h2(sprintf("Cluster %s", as.character(cluster)))
                 ## Numerator: cells in the current cluster.
                 numerator <- ident[which(ident == cluster)]
                 assert(all(numerator == cluster))
--- R/imports.R
+++ R/imports.R
@@ -41,7 +41,8 @@ NULL
 #'   melt metrics mutateIf organism organism<- printString sampleData
 #'   sampleData<- sampleNames separator showSlotInfo snakeCase
 #'   uniteInterestingGroups
-#' @importFrom cli cli_alert cli_alert_info cli_alert_warning cli_ul
+#' @importFrom cli cli_alert cli_alert_info cli_alert_warning cli_div cli_end
+#'   cli_h1 cli_h2 cli_ul
 #' @importFrom cowplot plot_grid
 #' @importFrom edgeR DGEList calcNormFactors estimateDisp glmFit glmLRT
 #' @importFrom ggplot2 aes element_rect expand_limits facet_grid facet_wrap
