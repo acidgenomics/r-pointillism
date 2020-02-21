e5a2fabe52398f289f4f737c420f4715bc6bb3e5
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-21 14:15:01 -0500

    Clean up deprecations

--- R/deprecated.R
+++ R/deprecated.R
@@ -7,74 +7,6 @@
 #' @keywords internal
 NULL
 
-#' @name defunct
-#' @inherit basejump::defunct
-#' @keywords internal
-NULL
-
-
-
-## v0.2.0 ======================================================================
-#' @rdname defunct
-#' @export
-knownMarkers <- function(...) {
-    .Defunct("KnownMarkers")
-}
-
-
-
-#' @rdname defunct
-#' @export
-knownMarkersDetected <- function(...) {
-    .Defunct("KnownMarkers")
-}
-
-
-
-#' @rdname defunct
-#' @export
-plotKnownMarkersDetected <- function(...) {
-    .Defunct("plotKnownMarkers")
-}
-
-
-
-#' @rdname defunct
-#' @export
-readCellTypeMarkers <- function(...) {
-    .Defunct("CellTypeMarkers")
-}
-
-
-
-#' @rdname defunct
-#' @export
-sanitizeMarkers <- function(...) {
-    .Defunct("SeuratMarkers")
-}
-
-
-
-## v0.2.5 ======================================================================
-#' @rdname defunct
-#' @export
-importCellCycleMarkersFromGoogle <- function(...) {
-    .Defunct("CellCycleMarkers")
-}
-
-
-#' @rdname defunct
-#' @export
-importCellTypeMarkersFromGoogle <- function(...) {
-    .Defunct("CellTypeMarkers")
-}
-
-#' @rdname defunct
-#' @export
-runZinbwave <- function(...) {
-    .Defunct()
-}
-
 
 
 ## v0.4.0 ======================================================================
@@ -93,7 +25,7 @@ NULL
 
 `plotDot,ANY` <-  # nolint
     function(object, ...) {
-        ## > .Deprecated("plotDots")
+        .Deprecated("plotDots")
         plotDots(object, ...)
     }
 
