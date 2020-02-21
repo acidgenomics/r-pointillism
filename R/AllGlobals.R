da4151ecdc6818f305920f13fd08a72f30e4af76
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-21 11:43:02 -0500

    Put internal formals into a single list object

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,59 +1,54 @@
 globalVariables(".")
 
-url <- "https://steinbaugh.com/pointillism"
-citation <- "citation(\"pointillism\")"
+.formalsList <- list(
+    BPPARAM = quote(BiocParallel::bpparam()),  # nolint
+    ## DR marker default color palettes.
+    darkMarkerColors =
+        ggplot2::scale_color_viridis_c(option = "plasma"),
+    lightMarkerColors =
+        ggplot2::scale_color_gradient(low = "gray90", high = "red"),
+    continuousColor =
+        quote(getOption(
+            "acid.continuous.color",
+            default = ggplot2::scale_color_gradient(
+                low = "gray75",
+                high = "purple"
+            )
+        )),
+    continuousColorPurpleOrange =
+        quote(getOption(
+            "acid.continuous.color",
+            default = ggplot2::scale_color_gradient2(
+                low = "orange",
+                mid = "gray75",
+                high = "purple",
+                midpoint = 0L
+            )
+        )),
+    discreteColor =
+        quote(getOption(
+            "acid.discrete.color",
+            default = acidplots::scale_color_synesthesia_d()
+        )),
+    dark = quote(getOption("acid.dark", default = FALSE)),
+    dims = quote(c(1L, 2L)),
+    direction = c("up", "down", "both"),
+    expression = c("mean", "sum"),
+    headerLevel = 2L,
+    labelClusters = quote(getOption("acid.label", default = TRUE)),
+    labelSize = quote(getOption("acid.labelSize", default = 6L)),
+    legend = quote(getOption("acid.legend", default = TRUE)),
+    pointAlpha = quote(getOption("acid.pointAlpha", default = 0.85)),
+    pointSize = quote(getOption("acid.pointSize", default = 0.75)),
+    pointsAsNumbers = quote(getOption("acid.pointsAsNumbers", default = FALSE)),
+    reduction = "UMAP"  # 1L
+)
 
-BPPARAM <- quote(BiocParallel::bpparam())  # nolint
-
-## DR marker default color palettes.
-darkMarkerColors <-
-    ggplot2::scale_color_viridis_c(option = "plasma")
-lightMarkerColors <-
-    ggplot2::scale_color_gradient(low = "gray90", high = "red")
-
-continuousColor <-
-    quote(getOption(
-        "acid.continuous.color",
-        default = ggplot2::scale_color_gradient(
-            low = "gray75",
-            high = "purple"
-        )
-    ))
-continuousColorPurpleOrange <-
-    quote(getOption(
-        "acid.continuous.color",
-        default = ggplot2::scale_color_gradient2(
-            low = "orange",
-            mid = "gray75",
-            high = "purple",
-            midpoint = 0L
-        )
-    ))
-discreteColor <-
-    quote(getOption(
-        "acid.discrete.color",
-        default = acidplots::scale_color_synesthesia_d()
-    ))
-
-dark <- quote(getOption("acid.dark", default = FALSE))
-dims <- quote(c(1L, 2L))
-direction <- c("up", "down", "both")
-expression <- c("mean", "sum")
-headerLevel <- 2L
-label <- quote(getOption("acid.label", default = TRUE))
-labelSize <- quote(getOption("acid.labelSize", default = 6L))
-legend <- quote(getOption("acid.legend", default = TRUE))
-pointAlpha <- quote(getOption("acid.pointAlpha", default = 0.85))
-pointSize <- quote(getOption("acid.pointSize", default = 0.75))
-pointsAsNumbers <-
-    quote(getOption("acid.pointsAsNumbers", default = FALSE))
-reduction <- "UMAP"  # 1L
-
-## Set default ggplot2 theme.
-if (isTRUE(getOption("acid.dark"))) {
-    theme <- acidplots::acid_theme_dark
-} else {
-    theme <- acidplots::acid_theme_light
-}
-theme <- theme(base_size = 14L, legend_position = "right")
-ggplot2::theme_set(new = theme)
+## > ## Set default ggplot2 theme.
+## > if (isTRUE(getOption("acid.dark"))) {
+## >     theme <- acidplots::acid_theme_dark
+## > } else {
+## >     theme <- acidplots::acid_theme_light
+## > }
+## > theme <- theme(base_size = 14L, legend_position = "right")
+## > ggplot2::theme_set(new = theme)
