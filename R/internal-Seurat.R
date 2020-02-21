ad584f06c6259a7b6858a99470bf6ff565202e03
Michael Steinbaugh <mike@steinbaugh.com>
2020-02-21 10:19:39 -0500

    Improve internal ident checks

--- R/internal-Seurat.R
+++ R/internal-Seurat.R
@@ -1,3 +1,4 @@
+## Updated 2020-02-21.
 .seuratCommand <-
     function(object, name, assay = NULL) {
         assert(
@@ -18,6 +19,7 @@
 
 
 
+## Updated 2020-02-21.
 .seuratCommandParam <-
     function(object, name, param, assay = NULL) {
         cmd <- .seuratCommand(object = object, name = name, assay = assay)
@@ -27,6 +29,43 @@
 
 
 
+#' Find which resolution is currently active in Seurat object.
+#'
+#' Internally this checks `Idents()` return against internal metadata columns
+#' slotted in `object@meta.data`.
+#'
+#' @note Updated 2020-02-21.
+#'
+#' @seealso
+#' - `Seurat::Idents()`
+#' - `Seurat:::Idents.Seurat`
+.seuratWhichResolution <- function(object) {
+    data <- object@meta.data
+    keep <- grepl("res\\.[.0-9]+$", colnames(data))
+    if (!any(keep)) {
+        stop("Failed to detect any cluster resolutions in `object@meta.data`.")
+    }
+    data <- data[, keep, drop = FALSE]
+    ## Now check against cluster idents currently returned by `Idents()`, which
+    ## are internally stashed in `object@active.ident`.
+    keep <- bapply(
+        X = data,
+        y = Idents(object),
+        FUN = function(x, y) {
+            identical(unname(x), unname(y))
+        }
+    )
+    if (!any(keep)) {
+        stop("Failed to match `Idents()` in `object@meta.data`.")
+    }
+    col <- names(keep)[keep]
+    assert(isString(col))
+    col
+}
+
+
+
+## Updated 2020-02-21.
 .seuratNormalizationMethod <- function(object, assay = NULL) {
     x <- .seuratCommandParam(
         object = object,
@@ -40,6 +79,7 @@
 
 
 
+## Updated 2020-02-21.
 .seuratScaleFactor <- function(object, assay = NULL) {
     x <- .seuratCommandParam(
         object = object,
