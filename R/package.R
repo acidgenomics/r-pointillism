7acafb169eb9832e5a4700451ded5b717ef61719
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-02 20:01:07 -0500

    NAMESPACE simplification

--- R/package.R
+++ R/package.R
@@ -15,21 +15,19 @@
 #' @importFrom AcidPlots !! !!! acid_theme_dark acid_theme_light matchLabels
 #'   plot_grid pretty_breaks sym syms
 #' @importFrom basejump DataFrame Gene2Symbol SingleCellExperiment
-#'   SplitDataFrameList alert as.data.frame as.matrix assay assay<- assayNames
-#'   assays assays<- camelCase capture.output cbind coerce colData colData<-
-#'   complete.cases convertGenesToSymbols counts counts<- decode do.call
-#'   estimateSizeFactors geometricMean head import interestingGroups
-#'   interestingGroups<- lapply leftJoin makeGene2SymbolFromEnsembl makeNames
-#'   mapGenesToIDs mapGenesToRownames mapGenesToSymbols markdownHeader
-#'   matchInterestingGroups mcols mcols<- melt metadata metadata<- metrics
-#'   model.matrix mutateIf organism organism<- na.omit packageName
-#'   packageVersion printString reducedDim reducedDim<- reducedDimNames
-#'   reducedDims relevel rowData rowData<- rowMeans rowRanges rowRanges<-
-#'   rowSums sampleData sampleData<- sampleNames separator session_info
-#'   showSlotInfo sizeFactors sizeFactors<- snakeCase split t table tail ul
-#'   uniteInterestingGroups unsplit verbatim
-#' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_alert_warning
-#'   cli_div cli_end cli_h1 cli_h2
+#'   SplitDataFrameList alert alertInfo alertSuccess alertWarning as.data.frame
+#'   as.matrix assay assay<- assayNames assays assays<- camelCase capture.output
+#'   cbind coerce colData colData<- complete.cases convertGenesToSymbols counts
+#'   counts<- decode do.call estimateSizeFactors geometricMean h1 h2 head import
+#'   interestingGroups interestingGroups<- lapply leftJoin
+#'   makeGene2SymbolFromEnsembl makeNames mapGenesToIDs mapGenesToRownames
+#'   mapGenesToSymbols markdownHeader matchInterestingGroups mcols mcols<- melt
+#'   metadata metadata<- metrics model.matrix mutateIf organism organism<-
+#'   na.omit packageName packageVersion printString reducedDim reducedDim<-
+#'   reducedDimNames reducedDims relevel rowData rowData<- rowMeans rowRanges
+#'   rowRanges<- rowSums sampleData sampleData<- sampleNames separator
+#'   session_info showSlotInfo sizeFactors sizeFactors<- snakeCase split t table
+#'   tail ul uniteInterestingGroups unsplit verbatim
 #' @importFrom dplyr group_by n summarize
 #' @importFrom edgeR DGEList calcNormFactors estimateDisp glmFit glmLRT
 #' @importFrom ggplot2 aes element_rect expand_limits facet_grid facet_wrap
