aa015e3721e59fb140891bf36c091e1fa34c9d69
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-02 19:34:47 -0500

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -8,8 +8,6 @@
 #'
 #' @importMethodsFrom basejump coerce
 #'
-#' @importFrom BiocGenerics counts counts<- rowMeans rowSums sizeFactors
-#'   sizeFactors<- table
 #' @importFrom DESeq2 DESeqDataSet DESeq estimateSizeFactorsForMatrix results
 #' @importFrom S4Vectors DataFrame as.data.frame as.matrix cbind complete.cases
 #'   do.call head lapply mcols mcols<- metadata metadata<- na.omit sapply split
@@ -19,19 +17,19 @@
 #' @importFrom SingleCellExperiment logcounts logcounts<- normcounts
 #'   normcounts<- reducedDim reducedDim<- reducedDimNames reducedDims
 #'   sizeFactors sizeFactors<-
-#' @importFrom SummarizedExperiment assay assay<- assayNames assays assays<-
-#'   colData colData<- rowData rowData<- rowRanges rowRanges<-
 #' @importFrom AcidPlots !! !!! acid_theme_dark acid_theme_light matchLabels
 #'   plot_grid sym syms
 #' @importFrom basejump Gene2Symbol SingleCellExperiment SplitDataFrameList
-#'   camelCase capture.output coerce convertGenesToSymbols decode
+#'   assay assay<- assayNames assays assays<- camelCase capture.output coerce
+#'   colData colData<- convertGenesToSymbols counts counts<- decode
 #'   estimateSizeFactors geometricMean import interestingGroups
 #'   interestingGroups<- leftJoin makeGene2SymbolFromEnsembl makeNames
 #'   mapGenesToIDs mapGenesToRownames mapGenesToSymbols markdownHeader
 #'   matchInterestingGroups melt metrics mutateIf organism organism<-
-#'   packageName packageVersion printString sampleData sampleData<- sampleNames
-#'   separator session_info showSlotInfo snakeCase uniteInterestingGroups
-#'   unsplit
+#'   packageName packageVersion printString rowData rowData<- rowMeans rowRanges
+#'   rowRanges<- rowSums sampleData sampleData<- sampleNames separator
+#'   session_info showSlotInfo sizeFactors sizeFactors<- snakeCase table
+#'   uniteInterestingGroups unsplit
 #' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_alert_warning
 #'   cli_div cli_dl cli_end cli_h1 cli_h2 cli_text cli_ul
 #' @importFrom dplyr group_by n summarize
@@ -49,9 +47,7 @@
 #'   isPositive isScalar isScalarNumeric isString isSubset validate
 #' @importFrom methods as getMethod is new setAs setClass setMethod
 #'   setReplaceMethod setValidity show slot slot<- validObject .hasSlot
-#' @importFrom scales percent pretty_breaks
 #' @importFrom scater calculateCPM normalizeCounts
-#' @importFrom stats median model.matrix relevel
 "_PACKAGE"
 
 
@@ -69,5 +65,7 @@
 #'   bpprogressbar<-
 #' @importFrom DelayedArray DelayedArray
 #' @importFrom DelayedMatrixStats colSums2 rowSums2
+#' @importFrom scales percent pretty_breaks
+#' @importFrom stats median model.matrix relevel
 #' @importFrom utils data
 NULL
