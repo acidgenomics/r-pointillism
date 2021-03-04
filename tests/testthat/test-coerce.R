b4abc88d51ca1ea23ab3ace99cdd09d008c6ba76
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-04 09:11:25 -0500

    Improve interconversion checks

--- tests/testthat/test-coerce.R
+++ tests/testthat/test-coerce.R
@@ -9,7 +9,40 @@ test_that("Seurat to SingleCellExperiment", {
     )
     expect_identical(
         object = reducedDimNames(x),
-        expected = c("pca", "tsne", "umap")
+        expected = c("PCA", "TSNE", "UMAP")
+    )
+    expect_identical(
+        object = colnames(reducedDim(x, type = "UMAP")),
+        expected = c("UMAP_1", "UMAP_2")
+    )
+    expect_identical(
+        object = colnames(colData(x)),
+        expected = c(
+            "orig.ident",
+            "nCount_RNA",
+            "nFeature_RNA",
+            "RNA_snn_res.0.8",
+            "letter.idents",
+            "groups",
+            "RNA_snn_res.1",
+            "ident"
+        )
+    )
+    expect_identical(
+        object = colnames(mcols(rowRanges(x))),
+        expected = c(
+            "broadClass",
+            "entrezId",
+            "geneBiotype",
+            "geneId",
+            "geneName",
+            "seqCoordSystem",
+            "vstMean",
+            "vstVariance",
+            "vstVarianceExpected",
+            "vstVarianceStandardized",
+            "vstVariable"
+        )
     )
     expect_identical(
         object = names(metadata(x)),
@@ -33,12 +66,21 @@ test_that("SingleCellExperiment to Seurat", {
 ## CANT CAMELCASE THE SCE, OTHERWISE WE GET DUPES IN SEURAT ARGH...
 test_that("SCE-seurat interconversion with subsetting", {
     a <- sce
+    colDataNames <- colnames(colData(a))
     ## Coerce to seurat.
     b <- as(a, "Seurat")
     expect_s4_class(b, "Seurat")
+    expect_identical(
+        object = colnames(colData(b)),
+        expected = colDataNames
+    )
     ## Coerce back to SCE.
     c <- as(b, "SingleCellExperiment")
     expect_s4_class(c, "SingleCellExperiment")
+    expect_identical(
+        object = colnames(colData(c)),
+        expected = colDataNames
+    )
     ## Subset to contain n-1 genes, n-1 cells.
     nr <- nrow(c) - 1L
     nc <- ncol(c) - 1L
@@ -49,4 +91,8 @@ test_that("SCE-seurat interconversion with subsetting", {
     e <- as(d, "Seurat")
     expect_s4_class(e, "Seurat")
     expect_identical(dim(counts(e)), c(nr, nc))
+    expect_identical(
+        object = colnames(colData(e)),
+        expected = colDataNames
+    )
 })
