80221ca16d5eef7e2cdb7fff1c2097b3ac3008bb
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-04 09:54:21 -0500

    Update documentation

--- R/package.R
+++ R/package.R
@@ -13,8 +13,8 @@
 #' @importFrom DESeq2 DESeqDataSet DESeq estimateSizeFactorsForMatrix results
 #' @importFrom Seurat CreateSeuratObject DefaultAssay GetAssayData Idents
 #'   NormalizeData Stdev VariableFeatures as.SingleCellExperiment as.Seurat
-#' @importFrom AcidPlots !! !!! acid_theme_dark acid_theme_light matchLabels
-#'   percent plot_grid pretty_breaks sym syms
+#' @importFrom AcidPlots !! acid_theme_dark acid_theme_light matchLabels percent
+#'   plot_grid pretty_breaks sym syms
 #' @importFrom basejump DataFrame Gene2Symbol SingleCellExperiment
 #'   SplitDataFrameList alert alertInfo alertSuccess alertWarning as.data.frame
 #'   as.matrix assay assay<- assayNames assays assays<- camelCase capture.output
@@ -55,5 +55,6 @@
 
 
 ## FIXME Can we take out this dependency?
+#' @importFrom AcidPlots !!!
 #' @importFrom dplyr group_by n summarize
 NULL
