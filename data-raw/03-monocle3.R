e9e4bd463d68ef5beec07de7482ca1ded8a28dfd
Michael Steinbaugh <mike@steinbaugh.com>
2021-07-16 10:55:41 -0400

    Update minimal cell_data_set example

--- data-raw/03-monocle3.R
+++ data-raw/03-monocle3.R
@@ -1,5 +1,5 @@
-## cell_data_set example
-## Updated 2020-01-18.
+## monocle3 cell_data_set example.
+## Updated 2021-07-16.
 
 ## This is currently failing to save with Bioconductor 3.10, due to changes in
 ## SingleCellExperiment that now cause validity checks to fail.
@@ -13,16 +13,17 @@
 ## - https://github.com/cole-trapnell-lab/monocle3/blob/master/examples/
 ##       c_elegans_embryo.R
 
+## FIXME Need to switch to lobstr here for object size checks.
+
 suppressPackageStartupMessages({
-    library(usethis)     # 1.5.1
-    library(pryr)        # 0.1.4
+    library(usethis)     # 2.0.1
     library(magrittr)    # 1.5
-    library(reticulate)  # 1.14
-    library(goalie)      # 0.4.1
-    library(basejump)    # 0.11.24
-    library(Matrix)      # 1.2-18
-    library(monocle3)    # 0.2.0
-    library(dplyr)       # 0.8.3
+    library(Matrix)      # 1.3-4
+    library(dplyr)       # 1.0.7
+    library(reticulate)  # 1.20
+    library(goalie)      # 0.5.1
+    library(basejump)    # 0.14.19
+    library(monocle3)    # 1.0.0
 })
 
 cores <- getOption("mc.cores")
@@ -33,15 +34,12 @@ virtualenv_list()
 use_virtualenv(virtualenv = "r-reticulate", required = TRUE)
 
 py_config()
-
-## macOS 10.14.6 (2020-01-18):
-##
-## python:         /usr/local/python/virtualenvs/r-reticulate/bin/python
-## libpython:      /Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/config-3.8-darwin/libpython3.8.dylib
-## pythonhome:     /Library/Frameworks/Python.framework/Versions/3.8:/Library/Frameworks/Python.framework/Versions/3.8
-## version:        3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53)  [Clang 6.0 (clang-600.0.57)]
-## numpy:          /usr/local/python/virtualenvs/r-reticulate/lib/python3.8/site-packages/numpy
-## numpy_version:  1.18.1
+## python:         /opt/koopa/opt/virtualenvs/r-reticulate/bin/python
+## libpython:      /Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/config-3.9-darwin/libpython3.9.dylib
+## pythonhome:     /opt/koopa/opt/virtualenvs/r-reticulate:/opt/koopa/opt/virtualenvs/r-reticulate
+## version:        3.9.6 (v3.9.6:db3ff76da1, Jun 28 2021, 11:14:58)  [Clang 12.0.5 (clang-1205.0.22.9)]
+## numpy:          /opt/koopa/opt/virtualenvs/r-reticulate/lib/python3.9/site-packages/numpy
+## numpy_version:  1.21.0
 
 ## Restrict object size to 2 MB.
 ## Use `pryr::object_size()` instead of `utils::object.size()`.
@@ -50,12 +48,15 @@ limit <- structure(2e6, class = "object_size")
 ## Load data ===================================================================
 prefix <- "http://staff.washington.edu/hpliner/data"
 
-expression_data <- readRDS(url(file.path(prefix, "cao_l2_expression.rds")))
-## > cell_metadata <- readRDS(url(file.path(prefix, "cao_l2_colData.rds")))
-gene_metadata <- readRDS(url(file.path(prefix, "cao_l2_rowData.rds")))
+## Don't time out after 60 seconds here.
+## The University of Washington staff website is currently pretty slow.
+options("timeout" = 600L)
+expression_data <- import(file.path(prefix, "cao_l2_expression.rds"))
+## > cell_metadata <- import(file.path(prefix, "cao_l2_colData.rds"))
+gene_metadata <- import(file.path(prefix, "cao_l2_rowData.rds"))
 
 head(colnames(expression_data))
-## This is bad, don't allow `-` in the names.
+## Don't allow "-" in the names, not syntactically valid.
 ## [1] "cele-001-001.CATGACTCAA" "cele-001-001.AAGACGGCCA"
 ## [3] "cele-001-001.GCCAACGCCA" "cele-001-001.ATAGGAGTAC"
 ## [5] "cele-001-001.CTCGTCTAGG" "cele-001-001.AAGTTGCCAT"
@@ -69,7 +70,7 @@ assert(
 )
 
 ## If you hit this error below, reinstall monocle3.
-## > install_github("cole-trapnell-lab/monocle3", force = TRUE)
+## > remotes::install_github("cole-trapnell-lab/monocle3", force = TRUE)
 ##
 ## Error in checkSlotAssignment(object, name, value) :
 ## 'reducedDims' is not a slot in class "SingleCellExperiment"
@@ -89,12 +90,12 @@ cds <- new_cell_data_set(
 counts <- counts(cds)
 
 topGenes <- counts %>%
-    Matrix::rowSums(.) %>%
+    rowSums() %>%
     sort(decreasing = TRUE) %>%
     head(n = 200L) %>%
     names()
 topCells <- counts %>%
-    Matrix::colSums(.) %>%
+    colSums() %>%
     sort(decreasing = TRUE) %>%
     head(n = 200L) %>%
     names()
@@ -106,18 +107,11 @@ rowData(cds) <- droplevels(rowData(cds))
 colData(cds) <- droplevels(colData(cds))
 
 slotNames(cds)
-##  [1] "preprocess_aux"      "reduce_dim_aux"
-##  [3] "principal_graph_aux" "principal_graph"
-##  [5] "clusters"            "int_elementMetadata"
-##  [7] "int_colData"         "int_metadata"
-##  [9] "reducedDims"         "rowRanges"
-## [11] "colData"             "assays"
-## [13] "NAMES"               "elementMetadata"
-## [15] "metadata"
-
-## If you don't specify rowData:
-## Warning: gene_metadata must contain a column verbatim named 'gene_short_name'
-## for certain functions.
+##  [1] "preprocess_aux"      "reduce_dim_aux"      "principal_graph_aux"
+##  [4] "principal_graph"     "clusters"            "int_elementMetadata"
+##  [7] "int_colData"         "int_metadata"        "rowRanges"
+## [10] "colData"             "assays"              "NAMES"
+## [13] "elementMetadata"     "metadata"
 
 colnames(colData(cds))
 ## [1] "cell"        "Size_Factor"
@@ -127,6 +121,8 @@ colnames(colData(cds))
 cds <- preprocess_cds(cds)
 
 ## Reduce dimensionality.
+## Note: reduce_dimension will produce slightly different output each time you
+## run it unless you set 'umap.fast_sgd = FALSE' and 'cores = 1'
 cds <- reduce_dimension(
     cds = cds,
     reduction_method = "UMAP",
@@ -134,9 +130,6 @@ cds <- reduce_dimension(
     umap.fast_sgd = TRUE,
     cores = cores
 )
-
-## Note: reduce_dimension will produce slightly different output each time you
-## run it unless you set 'umap.fast_sgd = FALSE' and 'cores = 1'
 
 ## Group cells into clusters.
 cds <- cluster_cells(
@@ -162,9 +155,7 @@ cds <- learn_graph(
     verbose = TRUE
 )
 
-object_size(cds)
-lapply(coerceS4ToList(cds), object_size)
-stopifnot(object_size(cds) < limit)
+stopifnot(obj_size(cds) < limit)
 
 cell_data_set <- cds
 use_data(cell_data_set, compress = "xz", overwrite = TRUE)
