47b0203bdb4e968c0ea98af5fbccb27e0feb8244
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-03 18:10:07 -0400

    Attempt to improve code coverage

--- tests/testthat/test-rmarkdown.R
+++ tests/testthat/test-rmarkdown.R
@@ -1,3 +1,36 @@
-context("R Markdown templates")
+context("R Markdown")
 
-## FIXME Need to render these here and ensure output is successful.
+## FIXME This seems to be hanging on "Importing _setup.R" step at end....
+
+test_that("Seurat per cluster analysis", {
+    skeleton <- system.file(
+        "rmarkdown",
+        "templates",
+        "seurat-per-cluster-analysis",
+        "skeleton",
+        "skeleton.Rmd",
+        package = .pkgName,
+        mustWork = TRUE
+    )
+    input <- tempfile(
+        pattern = "render",
+        tmpdir = tempdir(),
+        fileext = ".Rmd"
+    )
+    file.copy(from = skeleton, to = input, overwrite = TRUE)
+    out <- rmarkdown::render(
+        input = input,
+        output_format = "html_document",
+        clean = TRUE,
+        params = list(
+            "seurat_file" = system.file(
+                "data",
+                "Seurat.rda",
+                package = "AcidTest",
+                mustWork = TRUE
+            )
+        ),
+        quiet = TRUE
+    )
+    expect_true(file.exists(out))
+})
