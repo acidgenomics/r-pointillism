f433c32c73c5efe512a81bef63ff6101b090ce12
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-13 17:29:19 -0400

    Migrate markers to AcidSingleCell

--- R/data.R
+++ R/data.R
@@ -15,30 +15,6 @@
 
 
 
-#' Cell-cycle markers list
-#'
-#' @note Updated 2021-03-03.
-#' @return `list`.
-#' @usage data(cellCycleMarkersList)
-#' @examples
-#' data(cellCycleMarkersList)
-#' names(cellCycleMarkersList)
-"cellCycleMarkersList"
-
-
-
-#' Cell-type markers list
-#'
-#' @note Updated 2021-03-03.
-#' @return `list`.
-#' @usage data(cellTypeMarkersList)
-#' @examples
-#' data(cellTypeMarkersList)
-#' names(cellTypeMarkersList)
-"cellTypeMarkersList"
-
-
-
 #' Seurat all markers example
 #'
 #' @note Updated 2021-03-03.
