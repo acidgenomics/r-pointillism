9516a71d2ed0bbaac09445809295c405614c5961
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-13 09:53:12 -0400

    Improve consistency of globals

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,16 +1,16 @@
 .formalsList <- list(
-    BPPARAM = quote(BiocParallel::bpparam()),  # nolint
-    continuousColor =
+    "BPPARAM" = quote(BiocParallel::bpparam()),  # nolint
+    "continuousColor" =
         quote(getOption(
-            "acid.continuous.color",
+            x = "acid.continuous.color",
             default = ggplot2::scale_color_gradient(
                 low = "gray75",
                 high = "purple"
             )
         )),
-    continuousColorPurpleOrange =
+    "continuousColorPurpleOrange" =
         quote(getOption(
-            "acid.continuous.color",
+            x = "acid.continuous.color",
             default = ggplot2::scale_color_gradient2(
                 low = "orange",
                 mid = "gray75",
@@ -18,23 +18,52 @@
                 midpoint = 0L
             )
         )),
-    discreteColor =
+    "discreteColor" =
         quote(getOption(
-            "acid.discrete.color",
+            x = "acid.discrete.color",
             default = AcidPlots::scale_color_synesthesia_d()
         )),
-    dark = quote(getOption(x = "acid.dark", default = FALSE)),
-    dims = quote(c(1L, 2L)),
-    direction = c("both", "up", "down"),
-    expression = c("mean", "sum"),
-    headerLevel = 2L,
-    label = quote(getOption(x = "acid.label", default = TRUE)),
-    labelSize = quote(getOption(x = "acid.labelSize", default = 6L)),
-    legend = quote(getOption(x = "acid.legend", default = TRUE)),
-    pointAlpha = quote(getOption(x = "acid.pointAlpha", default = 0.85)),
-    pointSize = quote(getOption(x = "acid.pointSize", default = 0.75)),
-    pointsAsNumbers = quote(getOption(x = "acid.pointsAsNumbers", default = FALSE)),
-    reduction = "UMAP"  # 1L
+    "dark" =
+        quote(getOption(
+            x = "acid.dark",
+            default = FALSE
+        )),
+    "dims" = quote(c(1L, 2L)),
+    "direction" = c("both", "up", "down"),
+    "expression" = c("mean", "sum"),
+    "headerLevel" = 2L,
+    "label" =
+        quote(getOption(
+            x = "acid.label",
+            default = TRUE
+        )),
+    "labelSize" =
+        quote(getOption(
+            x = "acid.labelSize",
+            default = 6L
+        )),
+    "legend" =
+        quote(getOption(
+            x = "acid.legend",
+            default = TRUE
+        )),
+    "pointAlpha" =
+        quote(getOption(
+            x = "acid.pointAlpha",
+            default = 0.85
+        )),
+    "pointSize" =
+        quote(getOption(
+            x = "acid.pointSize",
+            default = 0.75
+        )),
+    "pointsAsNumbers" =
+        quote(getOption(
+            x = "acid.pointsAsNumbers",
+            default = FALSE
+        )),
+    ## Alternatively, consider using `1L` here instead.
+    "reduction" = "UMAP"
 )
 
 
