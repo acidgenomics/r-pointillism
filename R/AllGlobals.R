58d2bcca0ece681c1c5459f9742b1594eda0dc85
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-13 09:49:28 -0400

    Rework formalsList

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -23,17 +23,17 @@
             "acid.discrete.color",
             default = AcidPlots::scale_color_synesthesia_d()
         )),
-    dark = quote(getOption("acid.dark", default = FALSE)),
+    dark = quote(getOption(x = "acid.dark", default = FALSE)),
     dims = quote(c(1L, 2L)),
     direction = c("both", "up", "down"),
     expression = c("mean", "sum"),
     headerLevel = 2L,
-    label = quote(getOption("acid.label", default = TRUE)),
-    labelSize = quote(getOption("acid.labelSize", default = 6L)),
-    legend = quote(getOption("acid.legend", default = TRUE)),
-    pointAlpha = quote(getOption("acid.pointAlpha", default = 0.85)),
-    pointSize = quote(getOption("acid.pointSize", default = 0.75)),
-    pointsAsNumbers = quote(getOption("acid.pointsAsNumbers", default = FALSE)),
+    label = quote(getOption(x = "acid.label", default = TRUE)),
+    labelSize = quote(getOption(x = "acid.labelSize", default = 6L)),
+    legend = quote(getOption(x = "acid.legend", default = TRUE)),
+    pointAlpha = quote(getOption(x = "acid.pointAlpha", default = 0.85)),
+    pointSize = quote(getOption(x = "acid.pointSize", default = 0.75)),
+    pointsAsNumbers = quote(getOption(x = "acid.pointsAsNumbers", default = FALSE)),
     reduction = "UMAP"  # 1L
 )
 
@@ -42,17 +42,6 @@
 ## DR marker default color palettes.
 .darkMarkerColors <-
     ggplot2::scale_color_viridis_c(option = "plasma")
-
-
-
-## > ## Set default ggplot2 theme.
-## > if (isTRUE(getOption("acid.dark"))) {
-## >     theme <- AcidPlots::acid_theme_dark
-## > } else {
-## >     theme <- AcidPlots::acid_theme_light
-## > }
-## > theme <- theme(base_size = 14L, legend_position = "right")
-## > ggplot2::theme_set(new = theme)
 
 
 
