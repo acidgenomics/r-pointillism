a95890588074ba4469558e047de25d6115014fc3
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-11 11:56:40 -0400

    Add comments regarding annoying roxygen2 weirdness

--- R/data.R
+++ R/data.R
@@ -13,6 +13,14 @@
 
 
 
+## NOTE Hitting some roxygen weirdness with this object.
+##
+## Warning: The ncol() method for CompressedSplitDataFrameList objects is
+## deprecated. Please use ncols() on these objects instead.
+##
+## - https://github.com/r-lib/roxygen2/issues/595
+## - https://github.com/ComputationalProteomicsUnit/maker/issues/28
+
 #' Example `SeuratMarkersPerCluster` object
 #'
 #' @note Updated 2022-05-11.
